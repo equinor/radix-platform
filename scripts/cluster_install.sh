662c5035e5938992b38da12039c625d8f6ffed90
Dafferianto Trinugroho <daft@Dafferiantos-MacBook-Pro.local>
2019-01-08 12:24:50 +0100

    WIP

--- scripts/cluster_install.sh
+++ scripts/cluster_install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # PRECONDITIONS
 #
 # It is assumed that az, kubectl, and jq are installed
@@ -20,44 +22,50 @@
 #   NODE_VM_SIZE (defaulted if omitted)
 
 if [ -n "$CREDENTIALS_SECRET_NAME" ]; then
-    CREDENTIALS_SECRET_NAME = "credentials_new"
+    CREDENTIALS_SECRET_NAME="credentials-new"
 fi
 
 if [ -n "$KUBERNETES_VERSION" ]; then
-    KUBERNETES_VERSION = "1.11.5"
+    KUBERNETES_VERSION="1.11.5"
 fi
 
 if [ -n "$NODE_COUNT" ]; then
-    NODE_COUNT = "3"
+    NODE_COUNT="3"
 fi
 
 if [ -n "$NODE_VM_SIZE" ]; then
-    NODE_VM_SIZE = "Standard_DS4_v2"
+    NODE_VM_SIZE="Standard_DS4_v2"
 fi
 
 # Step 1: Download credentials from vault as sh script
+VAULT_NAME="radix-boot-dev-vault"
+CREDENTIALS_SECRET_NAME="credentials-new"
+echo "$VAULT_NAME"
+echo "$CREDENTIALS_SECRET_NAME"
 az keyvault secret show --vault-name "$VAULT_NAME" --name "$CREDENTIALS_SECRET_NAME" | jq -r .value > "./credentials.sh"
 
 # Step 2: Execute shell script to set environment variables
-./credentials.sh
+chmod +x ./credentials.sh
+cat ./credentials.sh
+source ./credentials.sh
 
 # Step 3: Create cluster
 echo "Creating azure kubernetes service ${CLUSTER_NAME}..." 
-command = "az aks create --resource-group "$RESOURCE_GROUP" --name "$CLUSTER_NAME" \
-    --no-ssh-key \
-    --kubernetes-version "$KUBERNETES_VERSION" \
-    --aad-server-app-id "$AAD_SERVER_APP_ID" \
-    --aad-server-app-secret "$AAD_SERVER_APP_SECRET" \
-    --aad-client-app-id "$AAD_CLIENT_APP_ID" \
-    --aad-tenant-id "$AAD_TENANT_ID" \
-    --service-principal "$SERVICE_PRINCIPAL" \
-    --client-secret "$CLIENT_SECRET" \
-    --node-count "$NODE_COUNT" \
-    --node-vm-size "$NODE_VM_SIZE""
+# command = "az aks create --resource-group "$RESOURCE_GROUP" --name "$CLUSTER_NAME" \
+#     --no-ssh-key \
+#     --kubernetes-version "$KUBERNETES_VERSION" \
+#     --aad-server-app-id "$AAD_SERVER_APP_ID" \
+#     --aad-server-app-secret "$AAD_SERVER_APP_SECRET" \
+#     --aad-client-app-id "$AAD_CLIENT_APP_ID" \
+#     --aad-tenant-id "$AAD_TENANT_ID" \
+#     --service-principal "$SERVICE_PRINCIPAL" \
+#     --client-secret "$CLIENT_SECRET" \
+#     --node-count "$NODE_COUNT" \
+#     --node-vm-size "$NODE_VM_SIZE""
 
 echo "Running command:"
 echo
-echo $command
+#echo $command
 
 $command
 
