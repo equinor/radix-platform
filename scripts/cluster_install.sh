2df47bc89c4f5c1338480d2535b72fa3a134618d
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-01-08 09:54:14 +0100

    Firt version

--- scripts/cluster_install.sh
+++ scripts/cluster_install.sh
@@ -1,21 +1,27 @@
 # PRECONDITIONS
 #
-# It is assumed that kubectl, jq... are installed
+# It is assumed that az, kubectl, and jq are installed
 #
 # PURPOSE
 #
-# The purpose of the shell script is to set up a cluster 
-# within a subscription
+# The purpose of the shell script is to set up a new cluster 
+# within a subscription on an existing resource group
 #
 # To run this script from terminal:
-# RESOURCE_GROUP=xx CLUSTER_NAME=yy ...  ./cluster_install.sh
+# VAULT_NAME=aa CREDENTIALS_SECRET_NAME=bb RESOURCE_GROUP=cc CLUSTER_NAME=dd KUBERNETES_VERSION=ee NODE_COUNT=ff NODE_VM_SIZE=gg  ./cluster_install.sh
 #
 # Input environment variables:
+#   VAULT_NAME
+#   CREDENTIALS_SECRET_NAME (defaulted if omitted)
 #   RESOURCE_GROUP
 #   CLUSTER_NAME
-#   KUBERNETES_VERSION
-#   NODE_COUNT
-#   NODE_VM_SIZE
+#   KUBERNETES_VERSION (defaulted if omitted)
+#   NODE_COUNT (defaulted if omitted)
+#   NODE_VM_SIZE (defaulted if omitted)
+
+if [ -n "$CREDENTIALS_SECRET_NAME" ]; then
+    CREDENTIALS_SECRET_NAME = "credentials_new"
+fi
 
 if [ -n "$KUBERNETES_VERSION" ]; then
     KUBERNETES_VERSION = "1.11.5"
@@ -29,9 +35,11 @@ if [ -n "$NODE_VM_SIZE" ]; then
     NODE_VM_SIZE = "Standard_DS4_v2"
 fi
 
-# Step 1: Download credentials_new from vault as sh script
+# Step 1: Download credentials from vault as sh script
+az keyvault secret show --vault-name "$VAULT_NAME" --name "$CREDENTIALS_SECRET_NAME" | jq -r .value > "./credentials.sh"
 
 # Step 2: Execute shell script to set environment variables
+./credentials.sh
 
 # Step 3: Create cluster
 echo "Creating azure kubernetes service ${CLUSTER_NAME}..." 
@@ -39,11 +47,11 @@ command = "az aks create --resource-group "$RESOURCE_GROUP" --name "$CLUSTER_NAM
     --no-ssh-key \
     --kubernetes-version "$KUBERNETES_VERSION" \
     --aad-server-app-id "$AAD_SERVER_APP_ID" \
-    --aad-server-app-secret "$(jq -r '.adServerAppSecret' $__credentials_path)" \
-    --aad-client-app-id "$(jq -r '.adClientAppId' $__credentials_path)" \
-    --aad-tenant-id "$(jq -r '.tenantId' $__credentials_path)" \
-    --service-principal "$(jq -r '.servicePrincipalId' $__credentials_path)" \
-    --client-secret "$(jq -r '.servicePrincipalSecret' $__credentials_path)" \
+    --aad-server-app-secret "$AAD_SERVER_APP_SECRET" \
+    --aad-client-app-id "$AAD_CLIENT_APP_ID" \
+    --aad-tenant-id "$AAD_TENANT_ID" \
+    --service-principal "$SERVICE_PRINCIPAL" \
+    --client-secret "$CLIENT_SECRET" \
     --node-count "$NODE_COUNT" \
     --node-vm-size "$NODE_VM_SIZE""
 
@@ -56,3 +64,5 @@ $command
 echo
 echo -e "Azure kubernetes service ${CLUSTER_NAME} created"
 
+# Step 4: Remove credentials file
+rm -f ./credentials.sh
