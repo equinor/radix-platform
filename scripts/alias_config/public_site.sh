10f2bf5e3978b8471d890eeaba8f639ff9d31d16
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-02-01 08:36:23 +0100

    Perform aliasing in a loop with cluster name as input

---
+++ scripts/alias_config/public_site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set alias script vars
+export RADIX_APP_CNAME="public-site-radix-platform-prod.$CLUSTER_NAME.radix.equinor.com"    # The CNAME you want to create an alias for
+export RADIX_APP_ALIAS_NAME="www"                                                           # The name of the alias
+export RADIX_APP_NAME="radix-platform"                                                      # The name of the app in the cluster
+export RADIX_APP_COMPONENT="public-site"                                                    # The component which should receive the traffic
+export RADIX_APP_COMPONENT_PORT="80"
--- scripts/create_alias.sh
+++ scripts/create_alias.sh
@@ -1,47 +1,67 @@
 #!/bin/bash
 
 # Example:
-# RADIX_ALIAS_CONFIG_VARS_PATH=./alias_config_console.sh ./create_alias.sh
+# CLUSTER_NAME=aa ./create_alias.sh
 
 # INPUTS:
-#   RADIX_ALIAS_CONFIG_VARS_PATH (Mandatory - path to file)
+#   CLUSTER_NAME                (Mandatory. Example: prod42)
+#   RADIX_ZONE_NAME             (Optional. Defaulted if omitted)
+#   RADIX_APP_ENVIRONMENT       (Optional. Defaulted if omitted. ex: "prod", "qa", "test")
+#   HELM_REPO                   (Optional. Defaulted if omitted)
 
 # Validate mandatory input
-if [[ -z "$RADIX_ALIAS_CONFIG_VARS_PATH" ]]; then
-    echo "Please provide RADIX_ALIAS_CONFIG_VARS_PATH. Value must be a path to a file."
+if [[ -z "$CLUSTER_NAME" ]]; then
+    echo "Please provide CLUSTER_NAME."
     exit 1
 fi
 
-# Import variables
-source ./"$RADIX_ALIAS_CONFIG_VARS_PATH"
+# Set default values for optional input
+if [[ -z "$RADIX_ZONE_NAME" ]]; then
+    RADIX_ZONE_NAME="radix.equinor.com"
+fi
 
-# Show what we got before starting on the The Great Work
-echo -e ""
-echo -e "Start creating alias using the following settings:"
-echo -e ""
-echo -e "RADIX_ZONE_NAME              : $RADIX_ZONE_NAME"
-echo -e "RADIX_APP_CNAME              : $RADIX_APP_CNAME"
-echo -e "RADIX_APP_ALIAS_NAME         : $RADIX_APP_ALIAS_NAME"
-echo -e "RADIX_APP_NAME               : $RADIX_APP_NAME"
-echo -e "RADIX_APP_ENVIRONMENT        : $RADIX_APP_ENVIRONMENT"
-echo -e "RADIX_APP_COMPONENT          : $RADIX_APP_COMPONENT"
-echo -e "RADIX_APP_COMPONENT_PORT     : $RADIX_APP_COMPONENT_PORT"
-echo -e "RADIX_HELM_REPO              : $RADIX_HELM_REPO"
-echo -e ""
+if [[ -z "$RADIX_APP_ENVIRONMENT" ]]; then
+    RADIX_APP_ENVIRONMENT="prod"
+fi
 
-# Create alias in the dns zone
-az network dns record-set cname set-record \
-    --resource-group common \
-    --zone-name "$RADIX_ZONE_NAME" \
-    --record-set-name "$RADIX_APP_ALIAS_NAME" \
-    --cname "$RADIX_APP_CNAME"
+if [[ -z "$HELM_REPO" ]]; then
+    HELM_REPO="radixprod"
+fi
 
-# Create ingress object in the cluster
-helm upgrade --install radix-ingress-"$RADIX_APP_ALIAS_NAME" "$RADIX_HELM_REPO"/ingress \
-    --version 1.0.3 \
-    --set aliasUrl="$RADIX_APP_ALIAS_NAME.$RADIX_ZONE_NAME" \
-    --set application="$RADIX_APP_NAME" \
-    --set applicationEnv="$RADIX_APP_ENVIRONMENT" \
-    --set component="$RADIX_APP_COMPONENT" \
-    --set componentPort="$RADIX_APP_COMPONENT_PORT" \
-    --set enableAutoTLS=true
+for filename in alias_config/*.sh; do
+    [ -e "$filename" ] || continue
+    
+    # Import variables
+    source ./"$filename"
+
+    # Show what we got before starting on the The Great Work
+    echo -e ""
+    echo -e "Start creating alias using the following settings:"
+    echo -e ""
+    echo -e "RADIX_ZONE_NAME              : $RADIX_ZONE_NAME"
+    echo -e "RADIX_APP_CNAME              : $RADIX_APP_CNAME"
+    echo -e "RADIX_APP_ALIAS_NAME         : $RADIX_APP_ALIAS_NAME"
+    echo -e "RADIX_APP_NAME               : $RADIX_APP_NAME"
+    echo -e "RADIX_APP_ENVIRONMENT        : $RADIX_APP_ENVIRONMENT"
+    echo -e "RADIX_APP_COMPONENT          : $RADIX_APP_COMPONENT"
+    echo -e "RADIX_APP_COMPONENT_PORT     : $RADIX_APP_COMPONENT_PORT"
+    echo -e "HELM_REPO                    : $HELM_REPO"
+    echo -e ""
+
+    # Create alias in the dns zone
+    az network dns record-set cname set-record \
+        --resource-group common \
+        --zone-name "$RADIX_ZONE_NAME" \
+        --record-set-name "$RADIX_APP_ALIAS_NAME" \
+        --cname "$RADIX_APP_CNAME"
+
+    # Create ingress object in the cluster
+    helm upgrade --install radix-ingress-"$RADIX_APP_ALIAS_NAME" "$RADIX_HELM_REPO"/ingress \
+        --version 1.0.3 \
+        --set aliasUrl="$RADIX_APP_ALIAS_NAME.$RADIX_ZONE_NAME" \
+        --set application="$RADIX_APP_NAME" \
+        --set applicationEnv="$RADIX_APP_ENVIRONMENT" \
+        --set component="$RADIX_APP_COMPONENT" \
+        --set componentPort="$RADIX_APP_COMPONENT_PORT" \
+        --set enableAutoTLS=true
+done
