eea1a18fb4482d0a5ce0b54b3e4d0b48fde9491e
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-03-11 13:52:18 +0100

    Use newest version

--- scripts/install_cluster.sh
+++ scripts/install_cluster.sh
@@ -65,7 +65,7 @@ if [[ -z "$RESOURCE_GROUP" ]]; then
 fi
 
 if [[ -z "$KUBERNETES_VERSION" ]]; then
-    KUBERNETES_VERSION="1.12.4"
+    KUBERNETES_VERSION="1.12.6"
 fi
 
 if [[ -z "$NODE_COUNT" ]]; then
