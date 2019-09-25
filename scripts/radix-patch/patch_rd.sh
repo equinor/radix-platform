8f3d0ef7a01bcd0f60e75afec6f41b8d8e065b8d
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-09-25 14:10:56 +0200

    Fix daft problems

--- scripts/radix-patch/patch_rd.sh
+++ scripts/radix-patch/patch_rd.sh
@@ -8,7 +8,7 @@
 DESTINATION_CLUSTER="$(kubectl config current-context)"
 
 echo ""
-echo "WARNIG!"
+echo "WARNING!"
 echo "This script is a tool for patching RadixDeployments to cater"
 echo "for changes in the Radix operator so that no customer application"
 echo "is broken."
@@ -18,7 +18,7 @@ echo ""
 
 read -p "Are you sure you want to continue? (Y/n) " really_sure
 if [[ $really_sure =~ (N|n) ]]; then
-  echo "Chicken!1"
+  echo "Chicken!"
   exit 1
 fi
 
