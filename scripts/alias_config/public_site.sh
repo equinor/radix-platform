b681ed17d309947292552c1969e8a55c00e2595b
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-10-23 13:11:40 +0200

    Add chart info to ingress

--- scripts/alias_config/public_site.sh
+++ scripts/alias_config/public_site.sh
@@ -4,6 +4,6 @@
 export RADIX_APP_CNAME="public-site-radix-platform-prod.$CLUSTER_NAME.$RADIX_ZONE_NAME"    # The CNAME you want to create an alias for
 export RADIX_APP_ALIAS_NAME="www"                                                          # The name of the alias
 export RADIX_APP_NAME="radix-platform"                                                     # The name of the app in the cluster
-unset RADIX_NAMESPACE                                                            # Use the radix app environment
+unset RADIX_NAMESPACE                                                                      # Use the radix app environment
 export RADIX_APP_COMPONENT="public-site"                                                   # The component which should receive the traffic
 export RADIX_APP_COMPONENT_PORT="80"
