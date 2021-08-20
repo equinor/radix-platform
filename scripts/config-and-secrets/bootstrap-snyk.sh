af864be2ac77aa329deeed91da8131b2ce59fe98
Nils Gustav Stråbø <nst@equinor.com>
2021-08-20 08:28:32 +0200

    define cluster and user prompt for snyk secret

--- scripts/config-and-secrets/bootstrap-snyk.sh
+++ scripts/config-and-secrets/bootstrap-snyk.sh
@@ -19,13 +19,17 @@
 
 # Required:
 # - RADIX_ZONE_ENV      : Path to *.env file
+# - CLUSTER_NAME        : Ex: "test-2", "weekly-93"
+
+# Optional:
+# - USER_PROMPT         : Is human interaction is required to run script? true/false. Default is true.
 
 #######################################################################################
 ### HOW TO USE
 ###
 
 # NORMAL
-# RADIX_ZONE_ENV=../radix-zone/radix_zone_dev.env ./bootstrap-snyk.sh
+# RADIX_ZONE_ENV=../radix-zone/radix_zone_dev.env CLUSTER_NAME="weekly-2" ./bootstrap-snyk.sh
 
 #######################################################################################
 ### START
@@ -71,6 +75,83 @@ else
     fi
     source "$RADIX_ZONE_ENV"
 fi
+
+if [[ -z "$CLUSTER_NAME" ]]; then
+    echo "Please provide CLUSTER_NAME" >&2
+    exit 1
+fi
+
+# Optional inputs
+
+if [[ -z "$USER_PROMPT" ]]; then
+    USER_PROMPT=true
+fi
+
+if [[ -z "$STAGING" ]]; then
+    STAGING=false
+fi
+
+#######################################################################################
+### Prepare az session
+###
+
+printf "Logging you in to Azure if not already logged in... "
+az account show >/dev/null || az login >/dev/null
+az account set --subscription "$AZ_SUBSCRIPTION_ID" >/dev/null
+printf "Done.\n"
+
+#######################################################################################
+### Verify task at hand
+###
+
+echo -e ""
+echo -e "Bootstrap of cert-manager will use the following configuration:"
+echo -e ""
+echo -e "   > WHERE:"
+echo -e "   ------------------------------------------------------------------"
+echo -e "   -  CLUSTER_NAME                     : $CLUSTER_NAME"
+echo -e "   -  RADIX_ZONE                       : $RADIX_ZONE"
+echo -e ""
+echo -e "   > WHAT:"
+echo -e "   -------------------------------------------------------------------"
+echo -e "   -  AZ_RESOURCE_KEYVAULT             : $AZ_RESOURCE_KEYVAULT"
+echo -e ""
+echo -e "   > WHO:"
+echo -e "   -------------------------------------------------------------------"
+echo -e "   -  AZ_SUBSCRIPTION                  : $(az account show --query name -otsv)"
+echo -e "   -  AZ_USER                          : $(az account show --query user.name -o tsv)"
+echo -e ""
+
+echo ""
+
+if [[ $USER_PROMPT == true ]]; then
+    read -p "Is this correct? (Y/n) " -n 1 -r
+    if [[ "$REPLY" =~ (N|n) ]]; then
+    echo ""
+    echo "Quitting."
+    exit 0
+    fi
+    echo ""
+fi
+
+echo ""
+
+#######################################################################################
+### Connect kubectl
+###
+
+# Exit if cluster does not exist
+printf "\nConnecting kubectl..."
+if [[ ""$(az aks get-credentials --overwrite-existing --admin --resource-group "$AZ_RESOURCE_GROUP_CLUSTERS"  --name "$CLUSTER_NAME" 2>&1)"" == *"ERROR"* ]]; then    
+    # Send message to stderr
+    echo -e "Error: Cluster \"$CLUSTER_NAME\" not found." >&2
+    exit 1        
+fi
+printf "...Done.\n"
+
+#######################################################################################
+### Apply SNYK access token secret
+###
 
 echo "access-token=$(az keyvault secret show -n radix-snyk-sa-access-token-$RADIX_ZONE --vault-name $AZ_RESOURCE_KEYVAULT|jq -r '.value')
     " > radix-snyk-sa-access-token.yaml
