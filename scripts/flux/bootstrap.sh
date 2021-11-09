d1550fb3757ab059b27863e3137333edf0753cd7
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-11-09 14:53:31 +0100

    improve get cluster ip

--- scripts/flux/bootstrap.sh
+++ scripts/flux/bootstrap.sh
@@ -257,21 +257,23 @@ kubectl create secret docker-registry radix-docker \
 rm -f sp_credentials.json
 printf "...Done\n"
 
-# list of AVAILABLE public ips assigned to the Radix Zone
+# list of public ips assigned to the cluster
 printf "Getting list of public ips assigned to $CLUSTER_NAME..."
-IPPRE_ID="/subscriptions/$AZ_SUBSCRIPTION_ID/resourceGroups/common/providers/Microsoft.Network/publicIPPrefixes/$IPPRE_NAME"
-ASSIGNED_IPS="$(az network public-ip list | jq '.[] | select(.publicIpPrefix.id=="'$IPPRE_ID'" and .ipConfiguration.resourceGroup=="MC_'$AZ_RESOURCE_GROUP_CLUSTERS'_'$CLUSTER_NAME'_'$AZ_INFRASTRUCTURE_REGION'")' | jq '.ipAddress' | jq -s '.')"
+ASSIGNED_IPS="$(az network public-ip list |
+    jq '.[] | select(.ipConfiguration.resourceGroup=="MC_'$AZ_RESOURCE_GROUP_CLUSTERS'_'$CLUSTER_NAME'_'$AZ_INFRASTRUCTURE_REGION'")' |
+    jq '{ipAddress: .ipAddress}' |
+    jq -s '.')"
 
 if [[ "$ASSIGNED_IPS" == "[]" ]]; then
-    printf " Found no IPs assigned to the cluster.\n"
+    echo "ERROR: Could not find Public IP of cluster."
 else
     # Loop through list of IPs and create a comma separated string. 
     for ippre in $(echo $ASSIGNED_IPS | jq -c '.[]')
     do
         if [[ -z $IP_LIST ]]; then
-            IP_LIST=$(echo $ippre | jq -r '.')
+            IP_LIST=$(echo $ippre | jq -r '.ipAddress')
         else
-            IP_LIST="$IP_LIST,$(echo $ippre | jq -r '.')"
+            IP_LIST="$IP_LIST,$(echo $ippre | jq -r '.ipAddress')"
         fi
     done
     printf "...Done\n"
