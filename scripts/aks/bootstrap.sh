d6dab5e15ae057099a875609f8005f5098b5a256
Svein-Petter Johnsen <sjohn@equinor.com>
2022-03-28 15:21:11 +0200

    Lock Prod Cluster and VNET

--- scripts/aks/bootstrap.sh
+++ scripts/aks/bootstrap.sh
@@ -483,6 +483,14 @@ az aks create "${AKS_BASE_OPTIONS[@]}" "${AKS_OMNIA_OPTIONS[@]}" "${AKS_ENV_OPTI
 echo "Done."
 
 #######################################################################################
+### Lock cluster and network resources
+###
+if [ "$RADIX_ENVIRONMENT" = "prod" ]; then
+az lock create --lock-type CanNotDelete --name $CLUSTER_NAME-lock --resource-group clusters --resource-type Microsoft.ContainerService/managedClusters --resource $CLUSTER_NAME  2>&1 >/dev/null
+az lock create --lock-type CanNotDelete --name $VNET_NAME-lock --resource-group clusters --resource-type Microsoft.Network/virtualNetworks --resource $VNET_NAME  2>&1 >/dev/null
+fi
+
+#######################################################################################
 ### Update local kube config
 ###
 
