5aa2d7765236a2140b94d797f6141cb8ef227b5f
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2022-04-29 10:49:02 +0200

    create dns role assignment with infra bootstrap

--- scripts/radix-zone/base-infrastructure/bootstrap.sh
+++ scripts/radix-zone/base-infrastructure/bootstrap.sh
@@ -392,6 +392,58 @@ function set_permissions_on_dns() {
     fi
 }
 
+function create_dns_role_definition_for_cert_manager() {
+    # Create DNS TXT Contributor role to be used by Cert Manager
+    CUSTOM_DNS_ROLE_JSON="cert-mananger-custom-dns-role.json"
+    test -f "$CUSTOM_DNS_ROLE_JSON" && rm "$CUSTOM_DNS_ROLE_JSON"
+    cat <<EOF >>${CUSTOM_DNS_ROLE_JSON}
+    {
+        "Name": "DNS TXT Contributor",
+        "Id": "",
+        "IsCustom": true,
+        "Description": "Can manage DNS TXT records only.",
+        "Actions": [
+            "Microsoft.Network/dnsZones/TXT/*",
+            "Microsoft.Network/dnsZones/read",
+            "Microsoft.Authorization/*/read",
+            "Microsoft.Insights/alertRules/*",
+            "Microsoft.ResourceHealth/availabilityStatuses/read",
+            "Microsoft.Resources/deployments/*",
+            "Microsoft.Resources/subscriptions/resourceGroups/read",
+            "Microsoft.Support/*"
+        ],
+        "AssignableScopes": [
+            "/subscriptions/16ede44b-1f74-40a5-b428-46cca9a5741b",
+            "/subscriptions/ded7ca41-37c8-4085-862f-b11d21ab341a"
+        ]
+    }
+EOF
+    ROLE_DEFINITION=$(az role definition list --name "DNS TXT Contributor" --query [].assignableScopes[] --output tsv)
+    if [[ -z ${ROLE_DEFINITION} ]]; then
+        printf "Creating DNS TXT Contributor role definition..."
+        az role definition create --role-definition "$CUSTOM_DNS_ROLE_JSON" 2>/dev/null
+        rm "$CUSTOM_DNS_ROLE_JSON"
+        while [ -z "$(az role definition list --query "[?roleName=='$ROLENAME'].name" -otsv)" ]; do
+            sleep 5
+            printf "."
+        done
+        printf "...Done.\n"
+    elif [[ ! ${ROLE_DEFINITION[@]} =~ ${AZ_SUBSCRIPTION_ID} ]]; then
+        echo "ERROR: Role definition exists, but subscription ${AZ_SUBSCRIPTION_ID} is not an assignable scope. This script does not update it, so it must be done manually."
+        return
+    else
+        echo "DNS TXT Contributor role definition exists."
+    fi
+
+    printf "Creating role assignment..."
+    az role assignment create \
+        --assignee "$APP_ID" \
+        --role "$ROLENAME" \
+        --scope "/subscriptions/${AZ_SUBSCRIPTION_ID}/resourceGroups/${AZ_RESOURCE_GROUP_COMMON}/providers/Microsoft.Network/dnszones/${AZ_RESOURCE_DNS}" \
+        2>/dev/null
+    printf "...Done.\n"
+
+}
 
 #######################################################################################
 ### System users
@@ -419,6 +471,7 @@ create_acr
 create_base_system_users_and_store_credentials
 set_permissions_on_acr
 set_permissions_on_dns
+create_dns_role_definition_for_cert_manager
 
 
 #######################################################################################
