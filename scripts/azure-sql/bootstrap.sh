5af24be7f4f7e4268c552bc91af5eb01015d0008
sondresjolyst <sosj@equinor.com>
2022-06-28 08:12:14 +0200

    script for creating storage account for sql logging

---
+++ scripts/azure-sql/bootstrap.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+#######################################################################################
+### PURPOSE
+###
+
+# Create storage account for sql servers
+
+#######################################################################################
+### INPUTS
+###
+
+# Required:
+# - RADIX_ZONE_ENV      : Path to *.env file
+
+#######################################################################################
+### HOW TO USE
+###
+# RADIX_ZONE_ENV=../radix-zone/radix_zone_dev.env ./bootstrap.sh
+
+#######################################################################################
+### Read inputs and configs
+###
+
+printf "Starting sql bootstrap...\n"
+
+# Required inputs
+
+if [[ -z "$RADIX_ZONE_ENV" ]]; then
+    echo "ERROR: Please provide RADIX_ZONE_ENV" >&2
+    exit 1
+else
+    if [[ ! -f "$RADIX_ZONE_ENV" ]]; then
+        echo "ERROR: RADIX_ZONE_ENV=$RADIX_ZONE_ENV is invalid, the file does not exist." >&2
+        exit 1
+    fi
+    source "$RADIX_ZONE_ENV"
+fi
+
+#######################################################################################
+### Create storage account
+###
+
+SQL_LOGS_STORAGEACCOUNT_EXIST=$(az storage account list \
+    --resource-group "$AZ_RESOURCE_GROUP_COMMON" \
+    --query "[0].name=='$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS'")
+
+if [ ! "$SQL_LOGS_STORAGEACCOUNT_EXIST" ]; then
+    printf "SQL logs storage account does not exists.\n"
+
+    printf "    Creating storage account %s" "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS"
+    az storage account create \
+        --name "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS" \
+        --resource-group "$AZ_RESOURCE_GROUP_COMMON" \
+        --location "$AZ_RADIX_ZONE_LOCATION" \
+        --sku "Standard_LRS" \
+        --subscription "$AZ_SUBSCRIPTION_ID" \
+        --only-show-errors
+    printf "Done.\n"
+else
+    printf "    Storage account exists.\n"
+fi
+
+MANAGEMENT_POLICY_EXIST=$(az storage account management-policy show \
+    --account-name "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS" \
+    --resource-group "$AZ_RESOURCE_GROUP_COMMON" \
+    --output none)
+
+LIFECYCLE=7
+
+POLICY_JSON="$(
+    cat <<END
+{
+  "rules": [
+      {
+          "enabled": "true",
+          "name": "sql-rule",
+          "type": "Lifecycle",
+          "definition": {
+              "actions": {
+                  "version": {
+                      "delete": {
+                          "daysAfterCreationGreaterThan": "$LIFECYCLE"
+                      }
+                  },
+              },
+              "filters": {
+                  "blobTypes": [
+                      "blockBlob"
+                  ],
+              }
+          }
+      }
+  ]
+}
+END
+)"
+
+if [ ! "$MANAGEMENT_POLICY_EXIST" ]; then
+printf "Storage account %s is missing policy\n" "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS"
+
+    if az storage account management-policy create \
+        --account-name "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS" \
+        --policy "$(echo "$POLICY_JSON")" \
+        --resource-group "$AZ_RESOURCE_GROUP_COMMON" \
+        --only-show-errors; then
+        printf "Successfully created policy for %s\n" "$AZ_RESOURCE_STORAGEACCOUNT_SQL_LOGS"
+    fi
+fi
+
+prinf "Done."
