ab354d3c917d209d3a7d9ae8da54f4557d88702d
sondresjolyst <sosj@equinor.com>
2022-06-28 09:34:49 +0200

    query fix

--- scripts/aks/bootstrap.sh
+++ scripts/aks/bootstrap.sh
@@ -368,7 +368,9 @@ else
 fi
 
 # NSG Flow Logs
-FLOW_LOGS_STORAGEACCOUNT_EXIST=$(az storage account list --resource-group "$AZ_RESOURCE_GROUP_LOGS" --query "[0].name=='$AZ_RESOURCE_STORAGEACCOUNT_FLOW_LOGS'")
+FLOW_LOGS_STORAGEACCOUNT_EXIST=$(az storage account list \
+    --resource-group "$AZ_RESOURCE_GROUP_LOGS" \
+    --query "[?name=='$AZ_RESOURCE_STORAGEACCOUNT_FLOW_LOGS'].name")
 FLOW_LOGS_STORAGEACCOUNT_ID="/subscriptions/$AZ_SUBSCRIPTION_ID/resourceGroups/$AZ_RESOURCE_GROUP_LOGS/providers/Microsoft.Storage/storageAccounts/$AZ_RESOURCE_STORAGEACCOUNT_FLOW_LOGS"
 
 if [ ! "$FLOW_LOGS_STORAGEACCOUNT_EXIST" ]; then
