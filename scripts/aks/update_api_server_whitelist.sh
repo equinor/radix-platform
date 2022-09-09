ee13efed2c44958da32a18856b08edc93534457c
Svein-Petter Johnsen <sjohn@equinor.com>
2022-09-09 12:27:31 +0200

    sort order of location

--- scripts/aks/update_api_server_whitelist.sh
+++ scripts/aks/update_api_server_whitelist.sh
@@ -126,8 +126,7 @@ function addwhitelist() {
 #######################################################################################
 ### Prepare K8S API IP WHITELIST
 ###
-
-MASTER_K8S_API_IP_WHITELIST=$(az keyvault secret show --vault-name "$AZ_RESOURCE_KEYVAULT" --name "$SECRET_NAME" --query="value" -otsv | base64 --decode 2>/dev/null)
+MASTER_K8S_API_IP_WHITELIST=$(az keyvault secret show --vault-name "$AZ_RESOURCE_KEYVAULT" --name "$SECRET_NAME" --query="value" -otsv | base64 --decode | jq '{whitelist:.whitelist | sort_by(.location | ascii_downcase)}' 2>/dev/null)
 CURRENT_K8S_API_IP_WHITELIST=()
 i=0
 fmt="%-8s%-33s%-12s\n"
