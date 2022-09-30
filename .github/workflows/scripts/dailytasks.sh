009e24e34acf275f92e090c66bb6da971dee8932
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2022-09-30 12:29:35 +0200

    Testrun

--- .github/workflows/scripts/dailytasks.sh
+++ .github/workflows/scripts/dailytasks.sh
@@ -7,6 +7,17 @@ if [[ -z "$TASK" ]]; then
     exit 1
 fi
 
+if [[ -z "$RADIX_ZONE_ENV" ]]; then
+    echo "ERROR: Please provide RADIX_ZONE_ENV" >&2
+    exit 1
+else
+    if [[ ! -f "$RADIX_ZONE_ENV" ]]; then
+        echo "ERROR: RADIX_ZONE_ENV=$RADIX_ZONE_ENV is invalid, the file does not exist." >&2
+        exit 1
+    fi
+    source "$RADIX_ZONE_ENV"
+fi
+
 function stopcluster() {
     while read -r list; do
         CLUSTER=$(jq -n "${list}" | jq -r .name)
@@ -14,7 +25,8 @@ function stopcluster() {
         STATUS=$(jq -n "${list}" | jq -r .powerstate)
         if [ "$STATUS" = "Running" ]; then
             printf "Stopping cluster $CLUSTER\n"
-            az aks stop --name $CLUSTER --resource-group $CGROUP --no-wait
+            #az aks stop --name $CLUSTER --resource-group $CGROUP --no-wait
+            curl -X POST -H 'Content-type: application/json' --data '{"text":"GitHub Action: Stopping cluster '"$CLUSTER"'"}' "$SLACK_WEBHOOK_URL"
         fi
     done < <(printf "%s" "${CLUSTERS}" | jq -c '.[].k8s[]')
 }
@@ -30,8 +42,9 @@ function startcluster() {
         fi
     done < <(printf "%s" "${CLUSTERS}" | jq -c '.[].k8s[]')
 }
-
+printf '%s %s\n' "$(date)"
 CLUSTERS=$(az aks list -ojson | jq '[{k8s:[.[] | select(.name | startswith("playground") | not) | {name: .name, resourceGroup: .resourceGroup, powerstate: .powerState.code}]}]')
+SLACK_WEBHOOK_URL="$(az keyvault secret show --vault-name "$AZ_RESOURCE_KEYVAULT" --name "$KV_SECRET_SLACK_WEBHOOK" | jq -r .value)"
 if [ "$TASK" = "stop" ]; then
     stopcluster
 elif [ "$TASK" = "start" ]; then
