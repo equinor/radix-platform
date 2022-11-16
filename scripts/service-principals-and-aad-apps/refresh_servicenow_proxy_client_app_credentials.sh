a789e65eda0dd4d512dbbe1975f52c6774536cf8
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2022-11-16 08:37:40 +0100

    Update scripts/service-principals-and-aad-apps/refresh_servicenow_proxy_client_app_credentials.sh
    Co-authored-by: Sondre Sjølyst <sonyslyst@gmail.com>

--- scripts/service-principals-and-aad-apps/refresh_servicenow_proxy_client_app_credentials.sh
+++ scripts/service-principals-and-aad-apps/refresh_servicenow_proxy_client_app_credentials.sh
@@ -65,7 +65,6 @@ az account show >/dev/null || az login >/dev/null
 az account set --subscription "$AZ_SUBSCRIPTION_ID" >/dev/null
 printf "Done.\n"
 
-
 #######################################################################################
 ### Verify task at hand
 ###
