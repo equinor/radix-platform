8acaaefdb25c40ca24d407b8b0141908e6c72df3
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2022-11-29 10:36:49 +0100

    create cluster with terraform

---
+++ terraform/aks/main.tf
@@ -0,0 +1,80 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  whitelist_ips = jsondecode(textdecodebase64("${data.azurerm_key_vault_secret.whitelist_ips.value}", "UTF-8"))
+  AZ_RESOURCE_GROUP_VNET_HUB = "cluster-vnet-hub-${var.RADIX_ZONE}"
+}
+
+data "azurerm_key_vault" "keyvault_env" {
+  name                = "radix-vault-dev"
+  resource_group_name = var.AZ_RESOURCE_GROUP_COMMON
+}
+
+data "azurerm_key_vault_secret" "whitelist_ips" {
+  name         = "kubernetes-api-server-whitelist-ips-dev"
+  key_vault_id = data.azurerm_key_vault.keyvault_env.id
+}
+
+resource "azurerm_resource_group" "clusters" {
+  name     = var.AZ_RESOURCE_GROUP_CLUSTERS
+  location = var.AZ_LOCATION
+}
+
+module "aks" {
+  source = "github.com/equinor/radix-terraform-azurerm-aks"
+
+  cluster_name = var.cluster_name
+  AZ_LOCATION  = var.AZ_LOCATION
+
+  # Resource groups
+  AZ_RESOURCE_GROUP_CLUSTERS = azurerm_resource_group.clusters.name
+  AZ_RESOURCE_GROUP_COMMON   = var.AZ_RESOURCE_GROUP_COMMON
+  AZ_RESOURCE_GROUP_VNET_HUB = local.AZ_RESOURCE_GROUP_VNET_HUB
+
+  # network
+  # AZ_PRIVATE_DNS_ZONES = var.AZ_PRIVATE_DNS_ZONES
+  whitelist_ips        = length(local.whitelist_ips.whitelist) != 0 ? [for x in local.whitelist_ips.whitelist : x.ip] : null
+
+  # AKS
+  aks_node_pool_name     = var.aks_node_pool_name
+  aks_node_pool_vm_size  = var.aks_node_pool_vm_size
+  aks_node_count         = var.aks_node_count
+  aks_kubernetes_version = var.aks_kubernetes_version
+
+  # Manage identity
+  MI_AKSKUBELET = var.MI_AKSKUBELET
+  MI_AKS        = var.MI_AKS
+
+  # Radix
+  RADIX_ZONE                     = var.RADIX_ZONE
+  RADIX_ENVIRONMENT              = var.RADIX_ENVIRONMENT
+  RADIX_WEB_CONSOLE_ENVIRONMENTS = var.RADIX_WEB_CONSOLE_ENVIRONMENTS
+}
+
+resource "azurerm_redis_cache" "redis_cache_web_console" {
+  count = length(var.RADIX_WEB_CONSOLE_ENVIRONMENTS)
+
+  name                          = "${var.cluster_name}-${var.RADIX_WEB_CONSOLE_ENVIRONMENTS[count.index]}"
+  resource_group_name           = var.AZ_RESOURCE_GROUP_CLUSTERS
+  location                      = var.AZ_LOCATION
+  capacity                      = "1"
+  family                        = "C"
+  sku_name                      = "Basic"
+  public_network_access_enabled = true
+  redis_configuration {
+    maxmemory_reserved              = "125"
+    maxfragmentationmemory_reserved = "125"
+    maxmemory_delta                 = "125"
+  }
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "cluster_link" {
+  count                 = length(var.AZ_PRIVATE_DNS_ZONES)
+  name                  = "${var.cluster_name}-link"
+  resource_group_name   = local.AZ_RESOURCE_GROUP_VNET_HUB
+  private_dns_zone_name = var.AZ_PRIVATE_DNS_ZONES[count.index]
+  virtual_network_id    = module.aks.vnet_cluster.id
+  registration_enabled  = false
+}
---
+++ terraform/aks/terraform.tfvars
@@ -0,0 +1,65 @@
+cluster_name  = "terraform-dev"
+whitelist_ips = [] # This will be set in main.tf
+
+#######################################################################################
+### AKS
+###
+
+aks_node_pool_name     = "nodepool1"
+aks_node_pool_vm_size  = "Standard_B4ms"
+aks_node_count         = 3
+aks_kubernetes_version = "1.23.8"
+
+#######################################################################################
+### Zone and cluster settings
+###
+
+AZ_LOCATION       = "northeurope"
+RADIX_ZONE        = "dev"
+RADIX_ENVIRONMENT = "dev"
+RADIX_WEB_CONSOLE_ENVIRONMENTS = ["qa", "prod"]
+
+#######################################################################################
+### Resource groups
+###
+
+AZ_RESOURCE_GROUP_CLUSTERS = "rg_tf_test" # original "clusters"
+AZ_RESOURCE_GROUP_COMMON   = "common"
+# AZ_RESOURCE_GROUP_MONITORING="monitoring"
+# AZ_RESOURCE_GROUP_LOGS="Logs-Dev"
+
+#######################################################################################
+### System users
+###
+
+MI_AKSKUBELET = [{
+  client_id = "117df4c6-ff5b-4921-9c40-5bea2e1c52d8"
+  id        = "/subscriptions/16ede44b-1f74-40a5-b428-46cca9a5741b/resourceGroups/common/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-radix-akskubelet-development-northeurope"
+  object_id = "89541870-e10a-403c-8d4c-d80e92dd5eb7"
+}]
+MI_AKS = [{
+  client_id = "1ff97b0f-f824-47d9-a98f-a045b6a759bc"
+  id        = "/subscriptions/16ede44b-1f74-40a5-b428-46cca9a5741b/resourceGroups/common/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-radix-aks-development-northeurope",
+  object_id = "7112e202-51f7-4fd2-b6a1-b944f14f0be3"
+}]
+
+AZ_PRIVATE_DNS_ZONES = [
+  "privatelink.database.windows.net",
+  "privatelink.blob.core.windows.net",
+  "privatelink.table.core.windows.net",
+  "privatelink.queue.core.windows.net",
+  "privatelink.file.core.windows.net",
+  "privatelink.web.core.windows.net",
+  "privatelink.dfs.core.windows.net",
+  "privatelink.documents.azure.com",
+  "privatelink.mongo.cosmos.azure.com",
+  "privatelink.cassandra.cosmos.azure.com",
+  "privatelink.gremlin.cosmos.azure.com",
+  "privatelink.table.cosmos.azure.com",
+  "privatelink.postgres.database.azure.com",
+  "privatelink.mysql.database.azure.com",
+  "privatelink.mariadb.database.azure.com",
+  "privatelink.vaultcore.azure.net",
+  "private.radix.equinor.com"
+]
+
