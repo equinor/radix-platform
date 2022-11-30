a6107d52a185c9da74c92c394184020a514ded81
sondresjolyst <sonyslyst@gmail.com>
2022-11-30 13:20:24 +0100

    Changed file structure

--- terraform/clusters/dev/sondre-dev/main.tf
+++ terraform/clusters/dev/sondre-dev/main.tf
@@ -5,6 +5,8 @@ provider "azurerm" {
 locals {
   whitelist_ips              = jsondecode(textdecodebase64("${data.azurerm_key_vault_secret.whitelist_ips.value}", "UTF-8"))
   AZ_RESOURCE_GROUP_VNET_HUB = "cluster-vnet-hub-${var.RADIX_ZONE}"
+  # cluster_name               = terraform.workspace
+  cluster_name               = var.cluster_name
 }
 
 data "azurerm_key_vault" "keyvault_env" {
@@ -25,7 +27,7 @@ resource "azurerm_resource_group" "rg_clusters" {
 module "aks" {
   source = "github.com/equinor/radix-terraform-azurerm-aks?ref=v0.1.0-alpha"
 
-  cluster_name = var.cluster_name
+  cluster_name = local.cluster_name
   AZ_LOCATION  = var.AZ_LOCATION
 
   # Resource groups
@@ -55,7 +57,7 @@ module "aks" {
 resource "azurerm_redis_cache" "redis_cache_web_console" {
   count = length(var.RADIX_WEB_CONSOLE_ENVIRONMENTS)
 
-  name                          = "${var.cluster_name}-${var.RADIX_WEB_CONSOLE_ENVIRONMENTS[count.index]}"
+  name                          = "${local.cluster_name}-${var.RADIX_WEB_CONSOLE_ENVIRONMENTS[count.index]}"
   resource_group_name           = azurerm_resource_group.rg_clusters.name
   location                      = var.AZ_LOCATION
   capacity                      = "1"
@@ -71,7 +73,7 @@ resource "azurerm_redis_cache" "redis_cache_web_console" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "cluster_link" {
   count                 = length(var.AZ_PRIVATE_DNS_ZONES)
-  name                  = "${var.cluster_name}-link"
+  name                  = "${local.cluster_name}-link"
   resource_group_name   = local.AZ_RESOURCE_GROUP_VNET_HUB
   private_dns_zone_name = var.AZ_PRIVATE_DNS_ZONES[count.index]
   virtual_network_id    = module.aks.vnet_cluster.id
