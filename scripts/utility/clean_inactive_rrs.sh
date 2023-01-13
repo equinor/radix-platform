d3b6626a00ddd52eb473f9e7f85d2b3a9f3ff87c
Magnus Longva (IT MSS LID) <magnus@longva.email>
2023-01-13 12:00:47 +0100

    script information is printed before execution

--- scripts/utility/clean_inactive_rrs.sh
+++ scripts/utility/clean_inactive_rrs.sh
@@ -14,6 +14,12 @@
 
 # First, set kubectl context to your desired cluster. Then run ./clean_inactive_rrs.sh
 
+#######################################################################################
+### HARDCODED PARAMETERS
+###
+
+INACTIVE_DAYS_BEFORE_STOP=7
+INACTIVE_DAYS_BEFORE_DELETION=28
 
 echo ""
 printf "Check for neccesary executables... "
@@ -57,7 +63,7 @@ function download_latest_rx_cleanup(){
 function prompt_for_confirmation() {
     while true; do
     echo ""
-    read -r -p "$1" yn
+    read -r -e -p "$1" yn
         case ${yn} in
         [Yy]*)
             break
@@ -74,17 +80,38 @@ function prompt_for_confirmation() {
 function list_and_stop_inactive_rrs() {
     dir=$1
     current_context=$(cat ~/.kube/config | grep "current-context:" | sed "s/current-context: //")
-    $dir/rx-cleanup list-rrs-for-stop
+    $dir/rx-cleanup list-rrs-for-stop --inactive-days-before-stop ${INACTIVE_DAYS_BEFORE_STOP}
     prompt_for_confirmation "Dry-run complete. Proceed with stopping these RadixRegistrations? Running in k8s context ${current_context} (Y/n) "
-    $dir/rx-cleanup stop-inactive-rrs
+    $dir/rx-cleanup stop-inactive-rrs --inactive-days-before-stop ${INACTIVE_DAYS_BEFORE_STOP}
 }
 
 function list_and_delete_inactive_rrs() {
     dir=$1
     current_context=$(cat ~/.kube/config | grep "current-context:" | sed "s/current-context: //")
-    $dir/rx-cleanup list-rrs-for-deletion
+    $dir/rx-cleanup list-rrs-for-deletion --inactive-days-before-deletion ${INACTIVE_DAYS_BEFORE_DELETION}
     prompt_for_confirmation "Dry-run complete. Proceed with deleting these RadixRegistrations? Running in k8s context ${current_context} (Y/n) "
-    $dir/rx-cleanup delete-inactive-rrs
+    $dir/rx-cleanup delete-inactive-rrs --inactive-days-before-deletion ${INACTIVE_DAYS_BEFORE_DELETION}
+}
+
+function print_script_information() {
+    message=$(cat << EndOfMessage
+    
+    This script is a wrapper around the rx-cleanup utility (https://github.com/equinor/radix-cluster-cleanup).
+    The purpose of this script is to power off and remove "inactive" RadixRegistrations in a Radix cluster, 
+    presumably the playground cluster. This script is configured to power off all components across all environments
+    in a RadixRegistration which has been inactive for >${INACTIVE_DAYS_BEFORE_STOP} days, and to delete the RadixRegistration entirely if it 
+    has been inactive for >${INACTIVE_DAYS_BEFORE_DELETION} days. In this context, "Activity" is either (1) a creation of a RadixJob, (2) a
+    restart of any component or (3) setting a RadixDeployment to 'active'.
+
+    The script has four steps. After each dry-run, you will be prompted for confirmation to wet-run :).
+    
+    (1) dry-run for stopping inactive RadixRegistrations
+    (2) stopping inactive RadixRegistrations
+    (3) dry-run for deleting inactive RadixRegistrations
+    (4) deleting inactive RadixRegistrations
+EndOfMessage
+    )
+    echo -e "\n${message}\n"
 }
 
 function verify_current_cluster() {
@@ -99,11 +126,11 @@ function clean-up-binary() {
   rm -r $bin_dir
 }
 
-
 bin_dir="/tmp/$(uuidgen)"
 # the trap function intercepts the SIGINT signal (CTRL+C) and cleans up downloaded binary before exiting
 trap "clean-up-binary $bin_dir && exit 2" 2
 download_latest_rx_cleanup $bin_dir
+print_script_information
 verify_current_cluster $bin_dir
 list_and_stop_inactive_rrs $bin_dir
 list_and_delete_inactive_rrs $bin_dir
