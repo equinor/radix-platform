b3efe97624d00ba6a52ba4f51d9b9fac0c374fb5
sondresjolyst <sonyslyst@gmail.com>
2023-02-13 11:11:23 +0100

    kv Access policies

---
+++ terraform/infrastructure/s941/dev/keyvaults/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azuread_service_principal" "SP_GITHUB_DEV_CLUSTER" {
+  application_id = var.SP_GITHUB_DEV_CLUSTER_CLIENT_ID
+}
+
+data "azurerm_key_vault" "RADIX_VAULT_DEV" {
+  name                = var.KV_RADIX_VAULT_DEV
+  resource_group_name = var.AZ_RESOURCE_GROUP_COMMON
+}
+
+resource "azurerm_key_vault_access_policy" "github_sp" {
+  key_vault_id = data.azurerm_key_vault.RADIX_VAULT_DEV.id
+  object_id    = data.azuread_service_principal.SP_GITHUB_DEV_CLUSTER.object_id
+  tenant_id    = var.AZ_TENANT_ID
+
+  secret_permissions = ["Get", "List", "Set"]
+}
