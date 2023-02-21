1d7d3ec559be5ab11ee92a97d5a2406b4e05bf55
sondresjolyst <sonyslyst@gmail.com>
2023-02-21 11:01:44 +0100

    universal names

--- terraform/infrastructure/s941/dev/keyvaults/main.tf
+++ terraform/infrastructure/s941/dev/keyvaults/main.tf
@@ -10,13 +10,13 @@ data "azuread_service_principal" "SP_GITHUB_ACTION_CLUSTER" {
   application_id = var.SP_GITHUB_ACTION_CLUSTER_CLIENT_ID
 }
 
-data "azurerm_key_vault" "RADIX_VAULT_DEV" {
+data "azurerm_key_vault" "RADIX_VAULT" {
   name                = var.KV_RADIX_VAULT
   resource_group_name = var.AZ_RESOURCE_GROUP_COMMON
 }
 
 resource "azurerm_key_vault_access_policy" "github_sp" {
-  key_vault_id = data.azurerm_key_vault.RADIX_VAULT_DEV.id
+  key_vault_id = data.azurerm_key_vault.RADIX_VAULT.id
   object_id    = data.azuread_service_principal.SP_GITHUB_ACTION_CLUSTER.object_id
   tenant_id    = var.AZ_TENANT_ID
 
