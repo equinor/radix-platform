226d379dc443b7899155a5e6994daaaa2abbfd44
sondresjolyst <sonyslyst@gmail.com>
2023-04-18 07:20:20 +0200

    update redis to version 6.0

--- scripts/redis/update_redis_cache_for_console.sh
+++ scripts/redis/update_redis_cache_for_console.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# PURPOSE
+#######################################################################################
+### PURPOSE
+###
+
 # Configures the redis cache for the cluster given the context.
 
-# Example 1:
-# RADIX_ZONE_ENV=./../radix-zone/radix_zone_dev.env AUTH_PROXY_COMPONENT="auth" CLUSTER_NAME="weekly-42" RADIX_WEB_CONSOLE_ENV="qa" ./update_redis_cache_for_console.sh
-
-# Example 2:
-# RADIX_ZONE_ENV=./../radix-zone/radix_zone_dev.env AUTH_PROXY_COMPONENT="auth" CLUSTER_NAME="weekly-49" RADIX_WEB_CONSOLE_ENV="prod" USER_PROMPT="false" ./update_redis_cache_for_console.sh
+#######################################################################################
+### INPUTS
+###
 
 # Required:
 # - RADIX_ZONE_ENV          : Path to *.env file
@@ -17,6 +18,45 @@
 
 # Optional:
 # - USER_PROMPT             : Enable/disable user prompt, ex: "true" [default], "false"
+
+#######################################################################################
+### HOW TO USE
+###
+
+# Example 1:
+# RADIX_ZONE_ENV=./../radix-zone/radix_zone_dev.env AUTH_PROXY_COMPONENT="auth" CLUSTER_NAME="weekly-42" RADIX_WEB_CONSOLE_ENV="qa" ./update_redis_cache_for_console.sh
+
+# Example 2:
+# RADIX_ZONE_ENV=./../radix-zone/radix_zone_dev.env AUTH_PROXY_COMPONENT="auth" CLUSTER_NAME="weekly-49" RADIX_WEB_CONSOLE_ENV="prod" USER_PROMPT="false" ./update_redis_cache_for_console.sh
+
+#######################################################################################
+### Check for prerequisites binaries
+###
+
+echo ""
+printf "Check for neccesary executables... "
+hash az 2>/dev/null || {
+    echo -e "\nERROR: Azure-CLI not found in PATH. Exiting... " >&2
+    exit 1
+}
+
+hash jq 2>/dev/null || {
+    echo -e "\nERROR: jq not found in PATH. Exiting..." >&2
+    exit 1
+}
+
+hash kubectl 2>/dev/null || {
+    echo -e "\nERROR: kubectl not found in PATH. Exiting... " >&2
+    exit 1
+}
+
+printf "Done.\n"
+
+#######################################################################################
+### Read inputs and configs
+###
+
+# Required inputs
 
 if [[ -z "$RADIX_ZONE_ENV" ]]; then
     echo "ERROR: Please provide RADIX_ZONE_ENV" >&2
@@ -44,14 +84,15 @@ if [[ -z "$RADIX_WEB_CONSOLE_ENV" ]]; then
     exit 1
 fi
 
+# Optional inputs
+
 if [[ -z "$USER_PROMPT" ]]; then
     USER_PROMPT=true
 fi
 
-
 # Source util scripts
 
-source ${RADIX_PLATFORM_REPOSITORY_PATH}/scripts/utility/util.sh
+source "${RADIX_PLATFORM_REPOSITORY_PATH}/scripts/utility/util.sh"
 
 #######################################################################################
 ### Prepare az session
@@ -59,21 +100,21 @@ source ${RADIX_PLATFORM_REPOSITORY_PATH}/scripts/utility/util.sh
 
 printf "Logging you in to Azure if not already logged in... "
 az account show >/dev/null || az login >/dev/null
-az account set --subscription "$AZ_SUBSCRIPTION_ID" >/dev/null
+az account set --subscription "${AZ_SUBSCRIPTION_ID}" >/dev/null
 printf "Done.\n"
 
 #######################################################################################
-### CLUSTER?
+### Connect kubectl
 ###
 
-kubectl_context="$(kubectl config current-context)"
-
-if [ "$kubectl_context" = "$CLUSTER_NAME" ] || [ "$kubectl_context" = "${CLUSTER_NAME}" ]; then
-    echo "kubectl is ready..."
-else
-    echo "ERROR: Please set your kubectl current-context to be ${CLUSTER_NAME}" >&2
-    exit 1
-fi
+# Exit if cluster does not exist
+printf "Connecting kubectl..."
+get_credentials "${AZ_RESOURCE_GROUP_CLUSTERS}" "${CLUSTER_NAME}" || {
+    # Send message to stderr
+    echo -e "ERROR: Cluster \"${CLUSTER_NAME}\" not found." >&2
+    exit 0        
+}
+printf "...Done.\n"
 
 #######################################################################################
 ### Verify cluster access
@@ -83,27 +124,28 @@ verify_cluster_access
 #######################################################################################
 
 function updateRedisCacheConfiguration() {
-    REDIS_CACHE_NAME="$CLUSTER_NAME-$RADIX_WEB_CONSOLE_ENV"
-    REDIS_CACHE_INSTANCE=$(az redis show --resource-group "$AZ_RESOURCE_GROUP_CLUSTERS" --name "$REDIS_CACHE_NAME" 2>/dev/null)
+    REDIS_CACHE_NAME="${CLUSTER_NAME}-${RADIX_WEB_CONSOLE_ENV}"
+    REDIS_CACHE_INSTANCE=$(az redis show --resource-group "${AZ_RESOURCE_GROUP_CLUSTERS}" --name "${REDIS_CACHE_NAME}" 2>/dev/null)
 
-    WEB_CONSOLE_NAMESPACE="radix-web-console-$RADIX_WEB_CONSOLE_ENV"
-    WEB_CONSOLE_AUTH_SECRET_NAME=$(kubectl get secret -l radix-component="$AUTH_PROXY_COMPONENT" -n "$WEB_CONSOLE_NAMESPACE" -ojson | jq -r .items[0].metadata.name)
-    OAUTH2_PROXY_REDIS_CONNECTION_URL="rediss://"$(jq -r '"\(.hostName):\(.sslPort)"' <<< $REDIS_CACHE_INSTANCE)
-    OAUTH2_PROXY_REDIS_PASSWORD=$(az redis list-keys --resource-group "$AZ_RESOURCE_GROUP_CLUSTERS" --name "$REDIS_CACHE_NAME" | jq -r .primaryKey)
-    REDIS_ENV_FILE="redis_secret_$REDIS_CACHE_NAME.env"
+    WEB_CONSOLE_NAMESPACE="radix-web-console-${RADIX_WEB_CONSOLE_ENV}"
+    WEB_CONSOLE_AUTH_SECRET_NAME=$(kubectl get secret -l radix-component="${AUTH_PROXY_COMPONENT}" --namespace "${WEB_CONSOLE_NAMESPACE}" --output json | jq -r .items[0].metadata.name)
+    OAUTH2_PROXY_REDIS_CONNECTION_URL="rediss://"$(jq -r '"\(.hostName):\(.sslPort)"' <<< ${REDIS_CACHE_INSTANCE})
+    OAUTH2_PROXY_REDIS_PASSWORD=$(az redis list-keys --resource-group "${AZ_RESOURCE_GROUP_CLUSTERS}" --name "${REDIS_CACHE_NAME}" | jq -r .primaryKey)
+    REDIS_ENV_FILE="redis_secret_${REDIS_CACHE_NAME}.env"
 
-    echo "OAUTH2_PROXY_REDIS_CONNECTION_URL=$OAUTH2_PROXY_REDIS_CONNECTION_URL" >> "$REDIS_ENV_FILE"
-    echo "OAUTH2_PROXY_REDIS_PASSWORD=$OAUTH2_PROXY_REDIS_PASSWORD" >> "$REDIS_ENV_FILE"
+    echo "OAUTH2_PROXY_REDIS_CONNECTION_URL=${OAUTH2_PROXY_REDIS_CONNECTION_URL}" >> "${REDIS_ENV_FILE}"
+    echo "OAUTH2_PROXY_REDIS_PASSWORD=${OAUTH2_PROXY_REDIS_PASSWORD}" >> "${REDIS_ENV_FILE}"
 
-    kubectl patch secret "$WEB_CONSOLE_AUTH_SECRET_NAME" --namespace "$WEB_CONSOLE_NAMESPACE" \
-        --patch "$(kubectl create secret generic "$WEB_CONSOLE_AUTH_SECRET_NAME" --namespace "$WEB_CONSOLE_NAMESPACE" --save-config --from-env-file="$REDIS_ENV_FILE" --dry-run=client -o yaml)"
+    kubectl patch secret "${WEB_CONSOLE_AUTH_SECRET_NAME}" \
+        --namespace "${WEB_CONSOLE_NAMESPACE}" \
+        --patch "$(kubectl create secret generic "${WEB_CONSOLE_AUTH_SECRET_NAME}" --namespace "${WEB_CONSOLE_NAMESPACE}" --save-config --from-env-file="${REDIS_ENV_FILE}" --dry-run=client --output yaml)"
 
-    rm "$REDIS_ENV_FILE"
+    rm "${REDIS_ENV_FILE}"
 
     echo "Redis Cache secrets updated"
 
-    printf "Restarting $AUTH_PROXY_COMPONENT deployment in $WEB_CONSOLE_NAMESPACE..."
-    kubectl rollout restart deployment -n "$WEB_CONSOLE_NAMESPACE" "$AUTH_PROXY_COMPONENT"
+    printf "Restarting %s deployment in %s..." "${AUTH_PROXY_COMPONENT}" "${WEB_CONSOLE_NAMESPACE}"
+    kubectl rollout restart deployment --namespace "${WEB_CONSOLE_NAMESPACE}" "${AUTH_PROXY_COMPONENT}"
     printf " Done.\n"
 }
 
