f1c1c1ab2210b4dc8779e3b65513c051420bb312
Sergey Smolnikov <ssmol@equinor.com>
2023-05-04 17:04:29 +0200

    Added commands. In progress

--- scripts/aks/create-federated-credentials.sh
+++ scripts/aks/create-federated-credentials.sh
@@ -1,42 +1,85 @@
 #!/usr/bin/env bash
+# Add new Managed identity federated credentials for new cluster to all service principals (both applications and managed identities, available for the user
+# ISSUER_URL=<issuer-url> ./create-federated-credentials.sh
+#
+# Get issuer URL:
+# az aks show --resource-group clusters --name <cluster-name> --query oidcIssuerProfile.issuerUrl
 
-echo ""
-printf "Check for necessary executables... "
+if [[ -z "${ISSUER_URL}" ]]; then
+  printf "ERROR: ISSUER_URL is not set.\n" >&2
+  exit 1
+fi
 
 #az login --use-device-code
 #../radix-cli/rrx get application -c development -o json|jq -r .name
+echo "Get an access token"
 token=$(az account get-access-token --resource 6dae42f8-4368-4678-94ff-3960e28e3630|jq -r .accessToken)
+if [[ -z "${token}" ]]; then
+  printf "ERROR: Could not get access token.\n" >&2
+  exit 1
+fi
 
-#using curl request json from api with bearer authorization token. Save json to env variable
+#Get available Radix applications
 appNames=$(curl -X GET \
   https://api.dev.radix.equinor.com/api/v1/applications \
   -H "Authorization: Bearer ${token}" \
   -H "Content-Type: application/json" \
   -H "cache-control: no-cache" 2>/dev/null |jq -r '.[].name')
+if [[ -z "${appNames}" ]]; then
+  printf "ERROR: Could not get applications.\n" >&2
+  exit 1
+fi
 
-identityEntries=()
+identityEntries=("test1" "test2")
+#Get identities for each application
 for appName in $appNames
 do
-  echo "--- app: $appName"
+  #echo "App: $appName"
+  if [[ "$appName" != "radix-test-fed-kv" ]]; then
+   # echo "  skip"
+    continue
+  fi
+
   envNames=$(curl -X GET \
           "https://api.dev.radix.equinor.com/api/v1/applications/${appName}/environments" \
           -H "Authorization: Bearer ${token}" \
           -H "Content-Type: application/json"  2>/dev/null |jq -r '.[].name')
+  if [[ -z "$envNames" ]]; then
+    echo " no environments found"
+    continue
+  fi
   for envName in $envNames
     do
-      echo "---- env: $envName"
+      if [[ "$envName" != "dev" ]]; then
+        echo "  skip"
+        continue
+      fi
+
       identityPropList=$(curl -X GET \
-              "https://api.dev.radix.equinor.com/api/v1/applications/${appName}/environments/${envName}" \
+          https://api.dev.radix.equinor.com/api/v1/applications/${appName}/environments/${envName} \
               -H "Authorization: Bearer ${token}" \
               -H "Content-Type: application/json" 2>/dev/null | \
-              jq -r '.activeDeployment as $i | select($i.components != null)| $i.components[] as $c|select($c.identity != null)|select($c.identity.azure != null)|{"ns":$i.namespace, "componentName":$c.name,"clientId":$c.identity.azure.clientId}')
+              jq -r '.activeDeployment as $d|select($d != null)|select($d.components != null)|$d.components[] as $c|select($c.identity != null)|select($c.identity.azure != null)|{namespace:$d.namespace,componentName:$c.name,clientId:$c.identity.azure.clientId}'|jq -c '.')
+
+      echo $identityPropList
+
       if [[ -z "$identityPropList" ]]; then
-        echo "------ no identity found"
-      else
-        identityEntries+=("$identityPropList")
+        echo "  no Azure identities for env: $envName"
+        continue
       fi
+
+      echo "  found Azure identities for env: $envName"
+      for identityProps in $identityPropList[@]
+      do
+        namespace="$(echo $identityProps|jq -r .namespace)"
+        componentName="$(echo $identityProps|jq -r .componentName)"
+        clientId="$(echo $identityProps|jq -r .clientId)"
+        echo "$namespace - $componentName - $clientId"
+      done
     done
 done
+
+echo "stop"
 
 #az ad app federated-credential list --id 7ef841f8-a263-45ea-8993-683cc6817ae2 -o json
 #az identity federated-credential list --identity-name serg-delete-me -g test-resources -o json
@@ -58,11 +101,11 @@ done
 
 #az ad sp list --show-mine --query "[?id=='c5a65135-...'][].{appId:appId,displayName:displayName,objectId:id,type:servicePrincipalType}"
 
-echo "Identity entries:"
-for identityEntry in "${identityEntries[@]}"
-do
-  echo "$identityEntry"
-done
+#echo "Identity entries:"
+#for identityEntry in "${identityEntries[@]}"
+#do
+#  echo "$identityEntry"
+#done
 
 
 #a='{"ns": "radix-test-fed-kv-dev","componentName": "app2","clientId": "7ef841f8-a263-45ea-8993-683cc6817ae2"}'
