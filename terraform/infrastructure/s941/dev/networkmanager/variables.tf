2d7c176ed76a173399c32d397082cfbee86c41f3
sondresjolyst <sonyslyst@gmail.com>
2023-11-17 13:03:09 +0100

    networkmanager deployment

--- terraform/infrastructure/s941/dev/networkmanager/variables.tf
+++ terraform/infrastructure/s941/dev/networkmanager/variables.tf
@@ -3,6 +3,16 @@ variable "AZ_SUBSCRIPTION_SHORTNAME" {
   type        = string
 }
 
+variable "AZ_SUBSCRIPTION_ID" {
+  description = "Azure subscription id"
+  type        = string
+}
+
+variable "AZ_LOCATION" {
+  description = "Azure location"
+  type        = string
+}
+
 variable "K8S_ENVIROMENTS" {
   description = "A map of cluster enviroments and their resource group"
   type        = map(object({
@@ -12,7 +22,7 @@ variable "K8S_ENVIROMENTS" {
 }
 
 variable "vnet_rg_names" {
-  type    = map(any)
+  type = map(any)
   default = {
     dev        = "cluster-vnet-hub-dev"
     playground = "cluster-vnet-hub-playground"
