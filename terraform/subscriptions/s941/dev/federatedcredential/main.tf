0b80d9fca537d4289c13dac3332d306fa97228ec
sondresjolyst <sonyslyst@gmail.com>
2023-12-13 14:02:47 +0100

    create MI and federated credential for each branch defined in gh_repos

--- terraform/subscriptions/s941/dev/federatedcredential/main.tf
+++ terraform/subscriptions/s941/dev/federatedcredential/main.tf
@@ -1,9 +1,15 @@
+locals {
+  userassignedidentities      = local.external_outputs.userassignedidentity.data
+  gh_repo_branch_combinations = local.external_outputs.global.data.gh_repo_branch_combinations
+}
+
 module "federatedcredential" {
+  for_each            = local.userassignedidentities
   source              = "../../../modules/federatedcredential"
-  parent_id           = local.external_outputs.userassignedidentity.data.id
+  parent_id           = each.value.data.id
   audiences           = ["api://AzureADTokenExchange"]
-  name                = "radix-canary-master"
+  name                = each.key
   issuer              = "https://token.actions.githubusercontent.com"
-  subject             = "repo:equinor/radix-canary:ref:refs/heads/master"
+  subject             = "repo:equinor/${local.gh_repo_branch_combinations[each.key].repo}:ref:refs/heads/${local.gh_repo_branch_combinations[each.key].branch}"
   resource_group_name = local.external_outputs.common.data.resource_group
 }
