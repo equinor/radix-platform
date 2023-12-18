29d253c4de635c56f6d8fcdfb9e1d947538ee116
Richard Hagen <richard.hagen@bouvet.no>
2023-12-18 16:04:04 +0100

    cleanup

--- scripts/acr/copy_acr_tool.sh
+++ scripts/acr/copy_acr_tool.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-repos=$(az acr repository list --name radixdev --output json)
+srcRegistry="radixdev"
+dstRegistry="radixdevdr"
+repos=$(az acr repository list --name "${srcRegistry}"  --output json)
 
 while read repo; do
   printf "Importing repo %s\n" "${repo}"
-  tags=$(az acr repository show-tags -n radixdev --repository "${repo}" --output json --orderby time_desc)
+  tags=$(az acr repository show-tags --name "${srcRegistry}" --repository "${repo}" --output json --orderby time_desc)
 
   while read tag; do
     printf "  tag %s\n" "${tag}"
-    az acr import -n radixdevdr --source "radixdev.azurecr.io/${repo}:${tag}"  --username import --password "<Source repo token password>" --no-wait
+    az acr import -n "${dstRegistry}" --source "${srcRegistry}.azurecr.io/${repo}:${tag}"  --username <source-username> --password "<source-password>" --no-wait
   done <<<"$(echo ${tags} | jq -r '.[0:10] | .[]')"
 
-done <<<"$(echo ${repos} | jq -r '.[]' | grep radix-)"
+done <<<"$(echo ${repos} | jq -r '.[]')"
