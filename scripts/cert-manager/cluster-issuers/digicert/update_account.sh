6622f798a6aff8732c9c51acd3bf2d69561a848f
Nils Gustav Stråbø <nst@equinor.com>
2023-12-18 10:27:26 +0100

    bootstrap digicert flux values

---
+++ scripts/cert-manager/cluster-issuers/digicert/update_account.sh
@@ -0,0 +1,175 @@
+#!/usr/bin/env bash
+
+
+#######################################################################################
+### PURPOSE
+### 
+
+# Store Digicert ACME external account info in Key Vault
+
+
+#######################################################################################
+### PRECONDITIONS
+### 
+
+# - User has permission to write key vault secret
+
+
+#######################################################################################
+### INPUTS
+### 
+
+# Required:
+# - RADIX_ZONE_ENV        : Path to *.env file
+# - ACME_ACCOUNT_KID      : ACME account key ID
+# - ACME_ACCOUNT_HMAC_KEY : ACME account HMAC key
+# - ACME_ACCOUNT_EMAIL    : ACME account email (must be the email registered for the Digicert account)
+# - ACME_SERVER           : ACME server URI
+
+# Optional:
+# - USER_PROMPT           : Is human interaction is required to run script? true/false. Default is true.
+
+
+#######################################################################################
+### HOW TO USE
+### 
+
+# Normal usage
+# RADIX_ZONE_ENV=../../../radix-zone/radix_zone_dev.env ACME_ACCOUNT_KID=<kid> ACME_ACCOUNT_HMAC_KEY=<hmac> ACME_ACCOUNT_EMAIL=any@equinor.com ACME_SERVER=https://acme.digicert.com/v2/acme/directory/ ./update_account.sh
+
+
+#######################################################################################
+### START
+### 
+
+echo ""
+echo "Start bootstrap of DigiCert secrets for Flux... "
+
+
+#######################################################################################
+### Check for prerequisites binaries
+###
+
+echo ""
+printf "Check for necessary executables... "
+hash az 2> /dev/null || { echo -e "\nERROR: Azure-CLI not found in PATH. Exiting..." >&2;  exit 1; }
+hash jq 2> /dev/null  || { echo -e "\nERROR: jq not found in PATH. Exiting..." >&2;  exit 1; }
+printf "All is good."
+echo ""
+
+
+#######################################################################################
+### Read inputs and configs
+###
+
+# Required inputs
+
+if [[ -z "$RADIX_ZONE_ENV" ]]; then
+    echo "ERROR: Please provide RADIX_ZONE_ENV" >&2
+    exit 1
+else
+    if [[ ! -f "$RADIX_ZONE_ENV" ]]; then
+        echo "ERROR: RADIX_ZONE_ENV=$RADIX_ZONE_ENV is invalid, the file does not exist." >&2
+        exit 1
+    fi
+    source "$RADIX_ZONE_ENV"
+fi
+
+if [[ -z "$ACME_ACCOUNT_KID" ]]; then
+    echo "ERROR: Please provide ACME_ACCOUNT_KID" >&2
+    exit 1
+fi
+
+if [[ -z "$ACME_ACCOUNT_HMAC_KEY" ]]; then
+    echo "ERROR: Please provide ACME_ACCOUNT_HMAC_KEY" >&2
+    exit 1
+fi
+
+if [[ -z "$ACME_ACCOUNT_EMAIL" ]]; then
+    echo "ERROR: Please provide ACME_ACCOUNT_EMAIL" >&2
+    exit 1
+fi
+
+if [[ -z "$ACME_SERVER" ]]; then
+    echo "ERROR: Please provide ACME_SERVER" >&2
+    exit 1
+fi
+
+# Optional inputs
+
+if [[ -z "$USER_PROMPT" ]]; then
+    USER_PROMPT=true
+fi
+
+#######################################################################################
+### Prepare az session
+###
+
+printf "Logging you in to Azure if not already logged in... "
+az account show >/dev/null || az login >/dev/null
+az account set --subscription "$AZ_SUBSCRIPTION_ID" >/dev/null
+printf "Done.\n"
+
+
+#######################################################################################
+### Verify task at hand
+###
+
+echo -e ""
+echo -e "Store Digicert ACME external account info in Key Vault will use the following configuration:"
+echo -e ""
+echo -e "   > WHERE:"
+echo -e "   ------------------------------------------------------------------"
+echo -e "   -  RADIX_ZONE                          : $RADIX_ZONE"
+echo -e "   -  AZ_RESOURCE_KEYVAULT                : $AZ_RESOURCE_KEYVAULT"
+echo -e "   -  DIGICERT_EXTERNAL_ACCOUNT_KV_SECRET : $DIGICERT_EXTERNAL_ACCOUNT_KV_SECRET"
+echo -e ""
+echo -e "   > WHAT:"
+echo -e "   -------------------------------------------------------------------"
+echo -e "   -  ACME_ACCOUNT_KID                    : $ACME_ACCOUNT_KID"
+echo -e "   -  ACME_ACCOUNT_HMAC_KEY               : <Redacted>"
+echo -e "   -  ACME_ACCOUNT_EMAIL                  : $ACME_ACCOUNT_EMAIL"
+echo -e "   -  ACME_SERVER                         : $ACME_SERVER"
+echo -e ""
+echo -e "   > WHO:"
+echo -e "   -------------------------------------------------------------------"
+echo -e "   -  AZ_SUBSCRIPTION                     : $(az account show --query name -otsv)"
+echo -e "   -  AZ_USER                             : $(az account show --query user.name -o tsv)"
+echo -e ""
+
+echo ""
+
+if [[ $USER_PROMPT == true ]]; then
+    while true; do
+        read -p "Is this correct? (Y/n) " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) echo ""; echo "Quitting."; exit 0;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+    echo ""
+fi
+
+#######################################################################################
+### Update Key Vault secret
+###
+
+printf "\nUpdating Digicert external account info in Key Vault...\n"
+
+secret=$(jq --null-input -r \
+    --arg accountKeyID "$ACME_ACCOUNT_KID" \
+    --arg accountHMACKey "$ACME_ACCOUNT_HMAC_KEY" \
+    --arg accountEmail "$ACME_ACCOUNT_EMAIL" \
+    --arg acmeServer "$ACME_SERVER" \
+    '{accountKeyID: $accountKeyID, accountHMACKey: $accountHMACKey, accountEmail: $accountEmail, acmeServer: $acmeServer}'
+) || exit
+
+az keyvault secret set --only-show-errors \
+    --vault-name "${AZ_RESOURCE_KEYVAULT}" \
+    --name "${DIGICERT_EXTERNAL_ACCOUNT_KV_SECRET}" \
+    --value "${secret}" \
+    2>&1 >/dev/null || exit
+
+echo ""
+printf "Updating Digicert external account info done!\n"
