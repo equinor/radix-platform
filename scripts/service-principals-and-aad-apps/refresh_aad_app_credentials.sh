e0b42461e05769e506613c295c83cfd30e7df634
Nils Gustav Stråbø <nst@equinor.com>
2024-01-04 08:49:26 +0100

    remove "Application Developer" role check

--- scripts/service-principals-and-aad-apps/refresh_aad_app_credentials.sh
+++ scripts/service-principals-and-aad-apps/refresh_aad_app_credentials.sh
@@ -106,8 +106,6 @@ az account show >/dev/null || az login >/dev/null
 az account set --subscription "$AZ_SUBSCRIPTION_ID" >/dev/null
 printf "Done.\n"
 
-exit_if_user_does_not_have_required_ad_role
-
 #######################################################################################
 ### Verify task at hand
 ###
