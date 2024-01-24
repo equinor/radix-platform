f6c66b6d0f8e2abacf7f3b2f9f74f074e445670c
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-01-24 13:06:01 +0100

    Added Vnets and Private DNS Zones

--- terraform/subscriptions/s941/playground/common/common.tf
+++ terraform/subscriptions/s941/playground/common/common.tf
@@ -2,6 +2,7 @@ locals {
   outputs = {
     location       = "northeurope"
     resource_group = "common"
+    enviroment     = "playground"
     enviroment_L   = "playground"
     enviroment_S   = "playground"
   }
--- terraform/subscriptions/s941/playground/common/variables.tf
+++ terraform/subscriptions/s941/playground/common/variables.tf
@@ -2,3 +2,26 @@ variable "resource_groups" {
   type    = list(string)
   default = ["common-playground"]
 }
+
+variable "storageaccounts" {
+  description = "Max 15 characters lowercase in the storageaccount name"
+  type = map(object({
+    name                     = string
+    resource_group_name      = optional(string, "common-playground")
+    location                 = optional(string, "northeurope")
+    account_tier             = optional(string, "Standard")
+    account_replication_type = optional(string, "LRS")
+    kind                     = optional(string, "StorageV2")
+    change_feed_enabled      = optional(bool, false)
+    versioning_enabled       = optional(bool, false)
+    roleassignment           = optional(map(object({ backup = optional(bool, false) })))
+    principal_id             = optional(string)
+    private_endpoint         = optional(bool, false)
+    firewall                 = optional(bool, true)
+  }))
+  default = {
+    radixlog = {
+      name = "radixlog"
+    }
+  }
+}
