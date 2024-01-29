60a8b4b0ce8f7a2c2b2758b13fc92fcf2b5d6fb2
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-01-29 15:09:51 +0100

    clientID removal

--- terraform/subscriptions/s940/extmon/virtualnetwork/backend.tf
+++ terraform/subscriptions/s940/extmon/virtualnetwork/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "azurerm" {
-    tenant_id            = "3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
-    subscription_id      = "ded7ca41-37c8-4085-862f-b11d21ab341a"
-    client_id            = "043e5510-738f-4c30-8b9d-ee32578c7fe8"
+    tenant_id       = "3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
+    subscription_id = "ded7ca41-37c8-4085-862f-b11d21ab341a"
+    #client_id            = "043e5510-738f-4c30-8b9d-ee32578c7fe8"
     resource_group_name  = "common-extmon"
     storage_account_name = "radixstateextmon"
     container_name       = "infrastructure"
