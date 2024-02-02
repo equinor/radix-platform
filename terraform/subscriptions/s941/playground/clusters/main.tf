834c3d9f12851099766aca57a385bebc417e0a8d
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-02-02 09:45:51 +0100

    server-operator

--- terraform/subscriptions/s941/playground/clusters/main.tf
+++ terraform/subscriptions/s941/playground/clusters/main.tf
@@ -4,3 +4,25 @@ module "resourcegroups" {
   name     = each.value
   location = local.outputs.location
 }
+
+module "policyassignment_resourcegroup" {
+  for_each             = module.resourcegroups
+  source               = "../../../modules/policyassignment_resourcegroup"
+  policy_name          = "Radix-Enforce-Diagnostics-AKS-Clusters"
+  location             = each.value["data"].location
+  resource_group_id    = each.value["data"].id
+  policy_definition_id = local.external_outputs.global.policy_aks_cluster_id
+  identity_ids         = local.external_outputs.common.mi_id
+  workspaceId          = local.external_outputs.common.workspace_id
+
+}
+
+
+module "nsg" {
+  source                     = "../../../modules/networksecuritygroup"
+  for_each                   = local.flattened_clusters
+  networksecuritygroupname   = "nsg-${each.key}"
+  location                   = each.value.location
+  resource_group_name        = each.value.resource_group_name
+  destination_address_prefix = each.value.destination_address_prefix
+}
--- terraform/subscriptions/s941/playground/clusters/variables.tf
+++ terraform/subscriptions/s941/playground/clusters/variables.tf
@@ -2,3 +2,14 @@ variable "resource_groups" {
   type    = list(string)
   default = ["clusters-playground"]
 }
+
+variable "clusters" {
+  type = map(object({
+    resource_group_name        = optional(string, "clusters")
+    location                   = optional(string, "northeurope")
+    destination_address_prefix = string
+  }))
+  default = {
+
+  }
+}
