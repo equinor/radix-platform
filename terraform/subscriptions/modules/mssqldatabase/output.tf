0e9a949d4e5c07fc7965ac846972ec5310353b58
Richard Hagen <richard.hagen@bouvet.no>
2024-02-05 18:06:46 +0100

    print out mi-client-id

--- terraform/subscriptions/modules/mssqldatabase/output.tf
+++ terraform/subscriptions/modules/mssqldatabase/output.tf
@@ -2,3 +2,10 @@ output "admin_adgroup" {
   description = "Admin AD Group Display Name"
   value       = data.azuread_group.admin.display_name
 }
+
+output "mi-admin" {
+  value = {
+    name = azurerm_user_assigned_identity.admin.name
+    client_id = azurerm_user_assigned_identity.admin.client_id
+  }
+}
