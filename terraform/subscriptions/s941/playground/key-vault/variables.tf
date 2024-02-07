386272909161491323ab4d3f90c9cac3a613e18a
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-02-07 15:15:37 +0100

    Remove unused code

--- terraform/subscriptions/s941/playground/key-vault/variables.tf
+++ terraform/subscriptions/s941/playground/key-vault/variables.tf
@@ -1,20 +1,16 @@
 variable "keyvaults" {
   description = "Key Vaults"
   type = map(object({
-    resource_group              = optional(string, "common-dev")
+    resource_group              = optional(string, "common-playground")
     soft_delete_retention_days  = optional(number, 30)
     enable_rbac_authorization   = optional(bool, false)
     purge_protection_enabled    = optional(bool, true)
     network_acls_default_action = optional(string, "Allow")
   }))
   default = {
-    radix-vault-dev = {
-      resource_group = "common"
+    radix-kv-playground = {
+      enable_rbac_authorization = true
     }
-    # radix-kv-dev = {
-    #   resource_group            = "common-dev"
-    #   enable_rbac_authorization = true
-    # }
   }
 
 }
