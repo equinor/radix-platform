ee79502bea22f04437e88f1ede150938eb7dec0b
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-02-12 07:40:26 +0100

    updates

--- terraform/subscriptions/s941/dev/post-clusters/main.tf
+++ terraform/subscriptions/s941/dev/post-clusters/main.tf
@@ -3,6 +3,19 @@ module "config" {
   source = "../../../modules/config"
 }
 
+resource "local_file" "templates" {
+    for_each = toset([
+        for file in fileset(path.module, "templates/**"): # The subfolder in current dir
+            file if length(regexall(".*app-template.*", file)) == 0 # Ignore paths with "app-template"
+    ])
+
+    content = templatefile(each.key, {
+        identity_id = data.azurerm_user_assigned_identity.this.client_id
+    })
+    
+    filename = replace("${path.module}/${each.key}", "templates", "rendered")
+}
+
 
 data "azurerm_kubernetes_cluster" "this" {
   for_each = toset(module.config.cluster_names)
