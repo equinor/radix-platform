214ba039530bc5a004d1cb8b094c113bdac176fa
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-03-05 14:44:08 +0100

    Modify path to Terraform and Velero patch

--- scripts/aks/bootstrap.sh
+++ scripts/aks/bootstrap.sh
@@ -962,9 +962,9 @@ fi
 ### Do some terraform post tasks
 ###
 echo "Do some terraform post tasks"
-
-terraform -chdir="../../terraform/subscriptions/$AZ_SUBSCRIPTION_NAME/$RADIX_ZONE/post-clusters" init
-terraform -chdir="../../terraform/subscriptions/$AZ_SUBSCRIPTION_NAME/$RADIX_ZONE/post-clusters" apply
+pwd
+terraform -chdir="../terraform/subscriptions/$AZ_SUBSCRIPTION_NAME/$RADIX_ZONE/post-clusters" init
+terraform -chdir="../terraform/subscriptions/$AZ_SUBSCRIPTION_NAME/$RADIX_ZONE/post-clusters" apply
 printf "Done."
 #######################################################################################
 ### END
