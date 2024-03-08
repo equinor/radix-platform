16225b04fbd806ef6ecdc831b1775f6877fa1e08
Richard87 <richard.hagen@gmail.com>
2024-03-08 13:20:15 +0100

    Create MI for Github workflow in Vulnaribility Scanner

--- terraform/subscriptions/modules/userassignedidentity/output.tf
+++ terraform/subscriptions/modules/userassignedidentity/output.tf
@@ -2,3 +2,8 @@ output "data" {
   description = "userassignedidentity"
   value       = azurerm_user_assigned_identity.userassignedidentity
 }
+
+output "client-id" {
+  description = "userassignedidentity"
+  value       = azurerm_user_assigned_identity.userassignedidentity.client_id
+}
