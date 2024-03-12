0cb7763b4aeac79204bacd265fd471958afda348
Richard87 <richard.hagen@gmail.com>
2024-03-12 08:43:15 +0100

    Added MI name to output

--- terraform/subscriptions/modules/userassignedidentity/output.tf
+++ terraform/subscriptions/modules/userassignedidentity/output.tf
@@ -7,3 +7,7 @@ output "client-id" {
   description = "userassignedidentity"
   value       = azurerm_user_assigned_identity.userassignedidentity.client_id
 }
+output "name" {
+  description = "Name of the new user assigned identity"
+  value = azurerm_user_assigned_identity.userassignedidentity.name
+}
