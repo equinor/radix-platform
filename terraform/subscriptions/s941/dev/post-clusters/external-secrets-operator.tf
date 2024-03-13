ab8dd9decd966f1817ffd788e450d835bd9b9d80
Richard Hagen <richard.hagen@gmail.com>
2024-03-13 08:02:09 +0100

    Add Workload Identities for Vulnerability Scanner (#1242)
    * Add Workload Identities for Vulnerability Scanner
    
    * add module
    
    * add mi reader
    
    * enabled all MI
    
    * Added MI name to output
    
    * move federated credentials to post-clusters
    
    * move federated credentials to post-clusters
    
    * Add fed-creds to Playground
    
    * Add changes to all environments
    
    * Add radixconfig to bootstrap script

--- terraform/subscriptions/s941/dev/post-clusters/external-secrets-operator.tf
+++ terraform/subscriptions/s941/dev/post-clusters/external-secrets-operator.tf
@@ -1,18 +1,3 @@
-module "config" {
-  source = "../../../modules/config"
-}
-
-data "azapi_resource_list" "clusters" {
-  type      = "Microsoft.ContainerService/managedClusters@2023-09-01"
-  parent_id = "/subscriptions/${module.config.backend.subscription_id}/resourcegroups/clusters" #TODO with code below after cluster in new RG
-  #parent_id              = "/subscriptions/${module.config.backend.subscription_id}/resourcegroups/${module.config.cluster_resource_group}"
-  response_export_values = ["*"]
-}
-
-locals {
-  clusters = { for k, v in jsondecode(data.azapi_resource_list.clusters.output).value : v.name => v.id }
-}
-
 resource "local_file" "templates" {
   for_each = toset([
     for file in fileset(path.module, "templates/**") :      # The subfolder in current dir
@@ -26,23 +11,16 @@ resource "local_file" "templates" {
   filename = replace("${path.module}/${each.key}", "templates", "rendered")
 }
 
-data "azurerm_kubernetes_cluster" "this" {
-  for_each            = local.clusters
-  resource_group_name = "clusters" #TODO with code below after cluster in new RG
-  #resource_group_name = module.config.cluster_resource_group
-  name = each.key
-}
-
 data "azurerm_user_assigned_identity" "this" {
   resource_group_name = module.config.common_resource_group
   name                = "radix-id-external-secrets-operator-${module.config.environment}"
 }
 
 resource "azurerm_federated_identity_credential" "eso" {
-  for_each = local.clusters
+  for_each = module.clusters.oidc_issuer_url
 
   audience            = ["api://AzureADTokenExchange"]
-  issuer              = data.azurerm_kubernetes_cluster.this[each.key].oidc_issuer_url
+  issuer              = each.value
   name                = "operator-wi-${each.key}"
   parent_id           = data.azurerm_user_assigned_identity.this.id
   resource_group_name = module.config.common_resource_group
