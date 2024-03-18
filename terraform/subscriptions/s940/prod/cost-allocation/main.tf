153a749902c8c48a05c07b67b547d1977c011c9d
Richard87 <richard.hagen@gmail.com>
2024-03-18 12:18:24 +0100

    Cost Allocation Managed ID: Added Platform IDs

--- terraform/subscriptions/s940/prod/cost-allocation/main.tf
+++ terraform/subscriptions/s940/prod/cost-allocation/main.tf
@@ -20,17 +20,21 @@ data "azurerm_key_vault_secret" "keyvault_secrets" {
 module "mssql-database" {
   source                        = "../../../modules/mssqldatabase"
   env                           = module.config.environment
-  managed_identity_admin_name   = "radix-id-cost-allocation-admin-platform"
   database_name                 = "sqldb-radix-cost-allocation"
-  server_name                   = "sql-radix-cost-allocation-prod" # ${module.config.environment} # See https://github.com/equinor/radix-platform/issues/1186
+  server_name                   = "sql-radix-cost-allocation-${module.config.environment}"
+  managed_identity_admin_name   = "radix-id-cost-allocation-admin-${module.config.environment}"
+  managed_identity_server_name  = "radix-id-cost-allocation-server-${module.config.environment}"
+  audit_storageaccount_name     = module.config.log_storageaccount_name
   admin_adgroup                 = var.admin-adgroup
+  azuread_authentication_only   = false
   administrator_login           = "radix"
   administrator_password        = data.azurerm_key_vault_secret.keyvault_secrets.value
   rg_name                       = module.resourcegroup.data.name
-  location                      = module.config.location
   vnet_resource_group           = module.config.vnet_resource_group
-  sku_name                      = "S3"
+  common_resource_group         = module.config.common_resource_group
+  location                      = module.config.location
   public_network_access_enabled = false
+  sku_name                      = "S3"
   zone_redundant                = false
   tags = {
     displayName = "SqlServer"
@@ -51,6 +55,64 @@ module "mssql-database" {
   }
 }
 
+
+data "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = "common" # TODO: Fix module.config.common_resource_group
+}
+
+module "github-workload-id" {
+  source              = "../../../modules/userassignedidentity"
+  name                = "radix-id-cost-allocation-github-${module.config.environment}"
+  resource_group_name = module.resourcegroup.data.name
+  location            = module.resourcegroup.data.location
+  roleassignments = {
+    contributor = {
+      role     = "Contributor" # Needed to open firewall
+      scope_id = data.azurerm_container_registry.acr.id
+    },
+  }
+  federated_credentials = {
+    github-main = {
+      name    = "gh-radix-cost-allocation-acr-main-${module.config.environment}"
+      issuer  = "https://token.actions.githubusercontent.com"
+      subject = "repo:equinor/radix-cost-allocation:ref:refs/heads/master"
+    }
+  }
+}
+
+module "mi-writer" {
+  source              = "../../../modules/userassignedidentity"
+  name                = "radix-id-cost-allocation-writer-${module.config.environment}"
+  resource_group_name = module.resourcegroup.data.name
+  location            = module.resourcegroup.data.location
+}
+
+module "mi-reader" {
+  source              = "../../../modules/userassignedidentity"
+  name                = "radix-id-cost-allocation-reader-${module.config.environment}"
+  resource_group_name = module.resourcegroup.data.name
+  location            = module.resourcegroup.data.location
+}
+
 output "mi-client-id" {
   value = module.mssql-database.mi-admin
+}
+output "github-buildpush-workflow" {
+  value = {
+    client-id = module.github-workload-id.client-id
+    name = module.github-workload-id.name
+  }
+}
+output "mi-writer" {
+  value = {
+    client-id = module.mi-writer.client-id,
+    name      = module.mi-writer.name
+  }
+}
+output "mi-reader" {
+  value = {
+    client-id = module.mi-reader.client-id,
+    name      = module.mi-reader.name
+  }
 }
