96e1cf9dee528555883254a1431cd5c3204356a9
Sergey Smolnikov <ssmol@equinor.com>
2024-06-12 16:29:25 +0200

    Corrected script

--- terraform/subscriptions/s941/dev/post-clusters/servicenow-api.tf
+++ terraform/subscriptions/s941/dev/post-clusters/servicenow-api.tf
@@ -3,22 +3,25 @@ data "azuread_application" "this" {
   display_name = "ar-radix-servicenow-proxy-client"
 }
 
-resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client-qa" {
-  for_each = module.clusters.oidc_issuer_url
-  application_id = data.azuread_application.this.id
-  display_name = "k8s-radix-servicenow-proxy-client-${each.key}-qa"
-  description  = "Application registration Federated Identity Credentials to access ServiceNow API"
-  audiences    = ["api://AzureADTokenExchange"]
-  issuer       = each.value
-  subject      = "system:serviceaccount:radix-servicenow-proxy-qa:api-sa"
+locals {
+  environments = ["prod", "qa"]
+  oidc_issuers = distinct(flatten([
+    for env in local.environments : [
+      for cluster, issuer in module.clusters.oidc_issuer_url : {
+        cluster = cluster
+        issuer = issuer
+        env = env
+      }
+    ]
+  ]))
 }
 
-resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client-prod" {
-  for_each = module.clusters.oidc_issuer_url
+resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client" {
+  for_each = { for entry in local.oidc_issuers: "${entry.cluster}-${entry.env}" => entry }
   application_id = data.azuread_application.this.id
-  display_name = "k8s-radix-servicenow-proxy-client-${each.key}-prod"
+  display_name = "k8s-radix-servicenow-proxy-client-${each.key}"
   description  = "Application registration Federated Identity Credentials to access ServiceNow API"
   audiences    = ["api://AzureADTokenExchange"]
-  issuer       = each.value
-  subject      = "system:serviceaccount:radix-servicenow-proxy-prod:api-sa"
+  issuer       = each.value.issuer
+  subject      = "system:serviceaccount:radix-servicenow-proxy-${each.value.env}:api-sa"
 }
