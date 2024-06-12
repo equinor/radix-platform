0336d24fd9bce70cdee12fd8d11033d691d54a33
Sergey Smolnikov <ssmol@equinor.com>
2024-06-12 15:51:03 +0200

    Added to terraform servicenow app registration and Federated Identity Credentials to access ServiceNow API for prod and qa

--- terraform/subscriptions/s941/dev/post-clusters/servicenow-api.tf
+++ terraform/subscriptions/s941/dev/post-clusters/servicenow-api.tf
@@ -3,13 +3,22 @@ data "azuread_application" "this" {
   display_name = "ar-radix-servicenow-proxy-client"
 }
 
-resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client" {
+resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client-qa" {
   for_each = module.clusters.oidc_issuer_url
   application_id = data.azuread_application.this.id
-  display_name = "k8s-radix-servicenow-proxy-client-${each.key}"
+  display_name = "k8s-radix-servicenow-proxy-client-${each.key}-qa"
   description  = "Application registration Federated Identity Credentials to access ServiceNow API"
   audiences    = ["api://AzureADTokenExchange"]
   issuer       = each.value
   subject      = "system:serviceaccount:radix-servicenow-proxy-qa:api-sa"
 }
 
+resource "azuread_application_federated_identity_credential" "ar-radix-servicenow-proxy-client-prod" {
+  for_each = module.clusters.oidc_issuer_url
+  application_id = data.azuread_application.this.id
+  display_name = "k8s-radix-servicenow-proxy-client-${each.key}-prod"
+  description  = "Application registration Federated Identity Credentials to access ServiceNow API"
+  audiences    = ["api://AzureADTokenExchange"]
+  issuer       = each.value
+  subject      = "system:serviceaccount:radix-servicenow-proxy-prod:api-sa"
+}
