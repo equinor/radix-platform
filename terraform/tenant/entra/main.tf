98b93bcdc1389ddc8013c23656c731af9bb40dc3
Sergey Smolnikov <ssmol@equinor.com>
2024-06-13 14:37:34 +0200

    use-workload-identity-for-servicenow (#1366)
    * Added to terraform servicenow app registration and Federated Identity Credentials to access ServiceNow API
    
    * Added to terraform servicenow app registration and Federated Identity Credentials to access ServiceNow API for prod and qa
    
    * Corrected script
    
    * Corrected script
    
    * Update terraform/tenant/entra/main.tf
    
    Co-authored-by: Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>

--- terraform/tenant/entra/main.tf
+++ terraform/tenant/entra/main.tf
@@ -30,3 +30,10 @@ resource "azurerm_role_assignment" "developer-roles" {
   scope                = data.azurerm_subscription.subscriptions[each.value.subscription].id
   role_definition_name = each.value.role
 }
+
+resource "azuread_application_registration" "ar-radix-servicenow-proxy-client" {
+  display_name = "ar-radix-servicenow-proxy-client"
+  service_management_reference = "110327"
+
+}
+
