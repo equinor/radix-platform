ec65d8a5a58cdeb75ae247ffd16e0f81536c2b3c
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2024-07-15 14:39:53 +0200

    Some post terraform updates (#1399)
    * Some post terraform updates
    
    * Updates
    
    * Updates
    
    ---------
    
    Co-authored-by: Automatic Update <radix@statoilsrm.onmicrosoft.com>

--- terraform/subscriptions/s940/prod/post-clusters/web-console.tf
+++ terraform/subscriptions/s940/prod/post-clusters/web-console.tf
@@ -31,6 +31,17 @@ locals {
   ))
 }
 
+data "azuread_application_published_app_ids" "well_known" {}
+data "azuread_service_principal" "servicenow" {
+  display_name = "ar-radix-servicenow-proxy-server"
+}
+data "azuread_service_principal" "msgraph" {
+  client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+}
+data "azuread_service_principal" "kubernetes" {
+  client_id = data.azuread_application_published_app_ids.well_known.result["AzureKubernetesServiceAadServer"]
+}
+
 module "webconsole" {
   source              = "../../../modules/app_registration"
   display_name        = "Omnia Radix Web Console - Platform" #TODO
@@ -40,4 +51,30 @@ module "webconsole" {
   owners              = data.azuread_group.radix.members
   implicit_grant      = false
   assignment_required = true
+
+  resource_access = {
+    servicenow = {
+      app_id = data.azuread_service_principal.servicenow.client_id
+      scope_ids = [
+        data.azuread_service_principal.servicenow.oauth2_permission_scope_ids["Application.Read"]
+      ]
+    }
+    kubernetes = {
+      app_id = data.azuread_application_published_app_ids.well_known.result["AzureKubernetesServiceAadServer"]
+      scope_ids = [
+        data.azuread_service_principal.kubernetes.oauth2_permission_scope_ids["user.read"],
+      ]
+    }
+    msgraph = {
+      app_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+      scope_ids = [
+        data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["GroupMember.Read.All"],
+        data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.Read"],
+        data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["profile"],
+        data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["openid"],
+        data.azuread_service_principal.msgraph.oauth2_permission_scope_ids["offline_access"],
+
+      ]
+    }
+  }
 }
