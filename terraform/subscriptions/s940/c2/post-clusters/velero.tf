11afbcb8dd25fd53ec55e6b4a005757059d51bc1
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2024-12-13 13:41:48 +0100

    updates

--- terraform/subscriptions/s940/c2/post-clusters/velero.tf
+++ terraform/subscriptions/s940/c2/post-clusters/velero.tf
@@ -20,10 +20,3 @@ resource "azurerm_storage_container" "velero" {
   storage_account_name  = "radixvelero${module.config.environment}"
   container_access_type = "private" # Options: private, blob, container
 }
-
-resource "azurerm_storage_container" "velero" {
-  for_each              = module.clusters.oidc_issuer_url
-  name                  = each.key
-  storage_account_name  = "radixvelero${module.config.environment}"
-  container_access_type = "private" # Options: private, blob, container
-}
