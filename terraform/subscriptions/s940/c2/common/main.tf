77347862cdc8be80722dbfce75121b95f336f3b0
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2024-12-19 08:23:39 +0100

    add missing fedcred to managed identities (#1554)
    * add missing fedcred to managed identities
    
    * fix incorrect fedcred

--- terraform/subscriptions/s940/c2/common/main.tf
+++ terraform/subscriptions/s940/c2/common/main.tf
@@ -112,7 +112,12 @@ module "radix-id-acr-workflows" {
       name    = "radix-acr-cleanup-release"
       issuer  = "https://token.actions.githubusercontent.com"
       subject = "repo:equinor/radix-acr-cleanup:ref:refs/heads/release"
-    }
+    },
+    radix-cluster-cleanup-release = {
+      name    = "radix-cluster-cleanup-release"
+      issuer  = "https://token.actions.githubusercontent.com"
+      subject = "repo:equinor/radix-cluster-cleanup:ref:refs/heads/release"
+    },
     radix-cicd-canary-release = {
       name    = "radix-cicd-canary-release"
       issuer  = "https://token.actions.githubusercontent.com"
