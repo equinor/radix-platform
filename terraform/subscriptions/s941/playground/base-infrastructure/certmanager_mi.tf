068779b7d74732d70f65cf776ec04747a10ff919
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2025-02-27 10:42:24 +0100

    Migrate DNZ zones to new RG (#1608)
    Co-authored-by: Automatic Update <radix@statoilsrm.onmicrosoft.com>

--- terraform/subscriptions/s941/playground/base-infrastructure/certmanager_mi.tf
+++ terraform/subscriptions/s941/playground/base-infrastructure/certmanager_mi.tf
@@ -1,17 +1,18 @@
-data "azurerm_dns_zone" "this" {
-  name                = "${module.config.environment}.radix.equinor.com"
-  resource_group_name = data.azurerm_resource_group.common.name
+module "dns_zone" {
+  source               = "../../../modules/dns_zone"
+  resourcegroup_common = module.resourcegroup_common.data.name
+  dnszoneprefix        = "${module.config.environment}."
 }
 
 module "radix-id-certmanager-mi" {
   source              = "../../../modules/userassignedidentity"
   name                = "radix-id-certmanager-${module.config.environment}"
   location            = module.config.location
-  resource_group_name = module.config.common_resource_group
+  resource_group_name = module.resourcegroup_common.data.name
   roleassignments = {
     role = {
       role     = "DNS TXT Contributor"
-      scope_id = data.azurerm_dns_zone.this.id
+      scope_id = module.dns_zone.azurerm_dns_zone_id
     }
   }
 }
