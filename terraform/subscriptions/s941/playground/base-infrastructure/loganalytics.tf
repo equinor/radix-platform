e46c0ab83f5b5554738b16aa6765bcb0f9503f69
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2025-02-28 14:49:21 +0100

    Terraform - Refactor Loganalytics and ACR

--- terraform/subscriptions/s941/playground/base-infrastructure/loganalytics.tf
+++ terraform/subscriptions/s941/playground/base-infrastructure/loganalytics.tf
@@ -1,8 +1,8 @@
 module "loganalytics" {
   source                        = "../../../modules/log-analytics"
   workspace_name                = "radix-logs-${module.config.environment}"
-  resource_group_name           = module.config.common_resource_group
-  location                      = module.config.location
+  resource_group_name           = module.resourcegroup_common.data.name
+  location                      = module.resourcegroup_common.data.location
   retention_in_days             = 30
   local_authentication_disabled = false
 }
