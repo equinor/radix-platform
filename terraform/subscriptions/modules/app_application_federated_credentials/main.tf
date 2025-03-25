0c0779b0e850c983678a23b0647d5c65c2efde23
Richard87 <richard.hagen@gmail.com>
2025-03-25 19:13:18 +0100

    chore: rebase

---
+++ terraform/subscriptions/modules/app_application_federated_credentials/main.tf
@@ -0,0 +1,7 @@
+resource "azuread_application_federated_identity_credential" "this" {
+  application_id                    = var.application_id
+  display_name   = var.display_name
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = var.issuer
+  subject        = var.subject
+}
