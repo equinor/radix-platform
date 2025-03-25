78c512cf208aa10e7b480c65a54436a5450d342a
Richard87 <richard.hagen@gmail.com>
2025-03-25 19:19:33 +0100

    chore: fmt

--- terraform/subscriptions/modules/app_application_federated_credentials/main.tf
+++ terraform/subscriptions/modules/app_application_federated_credentials/main.tf
@@ -1,5 +1,5 @@
 resource "azuread_application_federated_identity_credential" "this" {
-  application_id                    = var.application_id
+  application_id = var.application_id
   display_name   = var.display_name
   audiences      = ["api://AzureADTokenExchange"]
   issuer         = var.issuer
