adbb8cb28d45402ef3c26583cfefbec56e94b998
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2025-05-15 10:51:19 +0200

    Change GPU nodes spec (#1687)
    Co-authored-by: Automatic Update <radix@statoilsrm.onmicrosoft.com>

--- terraform/subscriptions/modules/aks/main.tf
+++ terraform/subscriptions/modules/aks/main.tf
@@ -56,23 +56,24 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 
   auto_scaler_profile {
-    balance_similar_node_groups      = false
-    empty_bulk_delete_max            = "10"
-    expander                         = "random"
-    max_graceful_termination_sec     = "600"
-    max_node_provisioning_time       = "15m"
-    max_unready_nodes                = 3
-    max_unready_percentage           = 45
-    new_pod_scale_up_delay           = "0s"
-    scale_down_delay_after_add       = "10m"
-    scale_down_delay_after_delete    = "10s"
-    scale_down_delay_after_failure   = "3m"
-    scale_down_unneeded              = "10m"
-    scale_down_unready               = "20m"
-    scale_down_utilization_threshold = "0.5"
-    scan_interval                    = "10s"
-    skip_nodes_with_local_storage    = false
-    skip_nodes_with_system_pods      = true
+    ignore_daemonsets_utilization_enabled = true
+    balance_similar_node_groups           = false
+    empty_bulk_delete_max                 = "10"
+    expander                              = "random"
+    max_graceful_termination_sec          = "600"
+    max_node_provisioning_time            = "15m"
+    max_unready_nodes                     = 3
+    max_unready_percentage                = 45
+    new_pod_scale_up_delay                = "0s"
+    scale_down_delay_after_add            = "10m"
+    scale_down_delay_after_delete         = "10s"
+    scale_down_delay_after_failure        = "3m"
+    scale_down_unneeded                   = "10m"
+    scale_down_unready                    = "20m"
+    scale_down_utilization_threshold      = "0.5"
+    scan_interval                         = "10s"
+    skip_nodes_with_local_storage         = false
+    skip_nodes_with_system_pods           = true
   }
 
   identity {
