d6ced01d3723108a4897bf588cdc3afa97c37c1d
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2025-05-27 13:21:03 +0200

    remove templates

--- terraform/templates/Readme_init_new_subscription/Readme.md
+++
@@ -1,2 +0,0 @@
-Create the Azure Terraform Backend [Azure Terraform Backend Template](https://github.com/equinor/azure-terraform-backend-template)
-
--- terraform/templates/zone_new/templates/base-infrastructure/backupvault.tf
+++
@@ -1,7 +0,0 @@
-module "backupvault" {
-  source                = "../../../modules/backupvaults"
-  name                  = "Backupvault-${prefix}{module.config.environment}"
-  resource_group_name   = module.resourcegroup_common.data.name
-  location              = module.config.location
-  policyblobstoragename = "Backuppolicy-blob"
-}
