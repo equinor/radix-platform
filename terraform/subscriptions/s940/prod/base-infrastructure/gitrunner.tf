8ae79e87489a4dd91b1e5c045931303ea249c761
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2025-07-24 14:40:53 +0200

    Remove old entry

--- terraform/subscriptions/s940/prod/base-infrastructure/gitrunner.tf
+++ terraform/subscriptions/s940/prod/base-infrastructure/gitrunner.tf
@@ -99,62 +99,6 @@ module "radix_id_gitrunner" {
       role     = "Locks Contributor"
       scope_id = "${data.azurerm_resource_group.clusters.id}" #TODO
     }
-    # privatelink-contributor = {
-    #   role     = "Radix Privatelink rbac-${module.config.subscription_shortname}"
-    #   scope_id = "/subscriptions/${module.config.subscription}"
-    # }
-    # blob_contributor = {
-    #   role     = "Contributor" # Needed to open firewall
-    #   scope_id = data.azurerm_storage_account.terraform_state.id
-    # }
-    # storage_blob_contributor = {
-    #   role     = "Storage Blob Data Contributor" # Needed to read blobdata
-    #   scope_id = data.azurerm_storage_account.terraform_state.id
-    # }
-    # common_contributor = {
-    #   role     = "Contributor" # Needed to open firewall
-    #   scope_id = module.resourcegroup_common.data.id
-    # }
-    # logs_contributor = {
-    #   role     = "Contributor"
-    #   scope_id = "${data.azurerm_resource_group.logs.id}" #TODO
-    # }
-    # clusters_contributor = {
-    #   role     = "Contributor"
-    #   scope_id = "${data.azurerm_resource_group.clusters.id}" #TODO
-    # }
-    # networkwatcher_contributor = {
-    #   role     = "Contributor"
-    #   scope_id = data.azurerm_resource_group.networkwatcher.id
-    # }
-    # keyvault_contributor = {
-    #   role     = "Key Vault Secrets User" # Needed to read secrets
-    #   scope_id = module.keyvault.vault_id
-    # }
-    # vnet_contributor = {
-    #   role     = "Contributor"
-    #   scope_id = module.resourcegroup_vnet.data.id
-    # }
-    # app_registry_contributor = {
-    #   role     = "Contributor"
-    #   scope_id = module.acr.azurerm_container_registry_app_id
-    # }
-    # vulnerability_scan_contributor = {
-    #   role     = "Managed Identity Contributor"
-    #   scope_id = module.resourcegroup_vulnerability_scan.data.id
-    # }
-    # cost_allocation_contributor = {
-    #   role     = "Managed Identity Contributor"
-    #   scope_id = module.resourcegroup_cost_allocation.data.id
-    # }
-    # grafana_contributor = {
-    #   role     = "Managed Identity Contributor"
-    #   scope_id = data.azurerm_resource_group.monitoring.id
-    # }
-    # lock_operator = {
-    #   role     = "Locks Contributor"
-    #   scope_id = "${data.azurerm_resource_group.clusters.id}" #TODO
-    # }
   }
   federated_credentials = {
     radix-id-gitrunner = {
