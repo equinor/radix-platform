53b3be542973f6ca06b0eb6bd72e415478bfdabe
Automatic Update <radix@statoilsrm.onmicrosoft.com>
2025-10-29 12:16:06 +0100

    Weekly-44 + App.reg ownership to AZ accounts

--- terraform/subscriptions/s940/extmon/base-infrastructure/appregistration.tf
+++ terraform/subscriptions/s940/extmon/base-infrastructure/appregistration.tf
@@ -4,7 +4,7 @@ module "app_application_registration" {
   displayname                        = each.value.display_name
   internal_notes                     = each.value.notes
   service_management_reference       = each.value.service_management_reference
-  radixowners                        = data.azuread_group.radix.members
+  radixowners                        = concat(data.azuread_group.radix.members, data.azuread_group.radix_az.members)
   permissions                        = each.value.permissions
   implicit_id_token_issuance_enabled = each.value.implicit_id_token_issuance_enabled
   app_role_assignment_required       = each.value.app_role_assignment_required
