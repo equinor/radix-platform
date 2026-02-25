0b5bd2f43c4c3bf0aeb9c536bb7aa5946534cea6
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2026-02-25 14:43:08 +0100

    Add feature for cluster-secret-store

--- terraform/subscriptions/s941/dev/pre-clusters/external-secrets-operator.tf
+++ terraform/subscriptions/s941/dev/pre-clusters/external-secrets-operator.tf
@@ -3,6 +3,20 @@ data "azurerm_user_assigned_identity" "this" {
   name                = "radix-id-external-secrets-operator-${module.config.environment}"
 }
 
+locals {
+  namespaces = ["default", "flux-system", "radix-cicd-canary"]
+  eso_namespaced_credentials = merge([
+    for cluster_key, issuer_url in module.clusters.oidc_issuer_url : {
+      for namespace in local.namespaces :
+      "${cluster_key}-${namespace}" => {
+        cluster_key = cluster_key
+        issuer_url  = issuer_url
+        namespace   = namespace
+      }
+    }
+  ]...)
+}
+
 module "eso" {
   source              = "../../../modules/federated-credentials"
   for_each            = module.clusters.oidc_issuer_url
@@ -13,3 +27,15 @@ module "eso" {
   resource_group_name = data.azurerm_user_assigned_identity.this.resource_group_name
   depends_on          = [module.aks]
 }
+
+module "eso_namespaced" {
+  source              = "../../../modules/federated-credentials"
+  for_each            = local.eso_namespaced_credentials
+  name                = "operator-wi-${each.value.cluster_key}-${each.value.namespace}"
+  issuer              = each.value.issuer_url
+  subject             = "system:serviceaccount:${each.value.namespace}:workload-identity-sa"
+  parent_id           = data.azurerm_user_assigned_identity.this.id
+  resource_group_name = data.azurerm_user_assigned_identity.this.resource_group_name
+  depends_on          = [module.aks]
+}
+
