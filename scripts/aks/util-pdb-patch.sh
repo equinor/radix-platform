c710f09c203040bcc55c4461067bc8acce0f1d06
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2026-08-10 10:40:56 +0200

    Updates

--- scripts/aks/util-pdb-patch.sh
+++ scripts/aks/util-pdb-patch.sh
@@ -13,16 +13,14 @@ if [[ -z "$CLUSTER_NAME" ]]; then
   exit 1
 fi
 
-clustername="$CLUSTER_NAME"
-
-kubectl --context "$clustername" cluster-info >/dev/null 2>&1 || {
-  echo "ERROR: Could not access cluster context $clustername. Quitting..." >&2
+kubectl --context "$CLUSTER_NAME" cluster-info >/dev/null 2>&1 || {
+  echo "ERROR: Could not access cluster context $CLUSTER_NAME. Quitting..." >&2
   exit 1
 }
 
-for row in $(kubectl --context "$clustername" get pdb -A -o json | jq -c '.items[] | select(.spec.minAvailable == 1) | {namespace: .metadata.namespace, name: .metadata.name, minAvailable: .spec.minAvailable}'); do
+for row in $(kubectl --context "$CLUSTER_NAME" get pdb -A -o json | jq -c '.items[] | select(.spec.minAvailable == 1) | {namespace: .metadata.namespace, name: .metadata.name, minAvailable: .spec.minAvailable}'); do
   namespace=$(echo "$row" | jq -r '.namespace')
   name=$(echo "$row" | jq -r '.name')
   minAvailable=$(echo "$row" | jq -r '.minAvailable')
-  kubectl --context "$clustername" patch pdb -n ${namespace} ${name} -p '{"spec":{"minAvailable":0}}'
+  kubectl --context "$CLUSTER_NAME" patch pdb -n ${namespace} ${name} -p '{"spec":{"minAvailable":0}}'
 done
