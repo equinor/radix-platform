4c5d68d922f45af96c3763626623ffa20d16862f
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-09-23 09:34:46 +0200

    feat: update max_count for nodepools_v1 to enhance scaling capabilities (#2072)

--- terraform/subscriptions/s940/c2/pre-clusters/variables.tf
+++ terraform/subscriptions/s940/c2/pre-clusters/variables.tf
@@ -170,7 +170,7 @@ variable "nodepools_v1" {
     armpipepool = {
       vm_size   = "Standard_E16ps_v6"
       min_count = 1
-      max_count = 2
+      max_count = 8
       node_labels = {
         "nodepooltasks" = "jobs"
       }
@@ -179,13 +179,13 @@ variable "nodepools_v1" {
     armuserpool = {
       vm_size   = "Standard_E16ps_v6"
       min_count = 1
-      max_count = 4
+      max_count = 16
 
     }
     x86pipepool = {
       vm_size   = "Standard_E16as_v7"
       min_count = 1
-      max_count = 2
+      max_count = 32
       node_labels = {
         "nodepooltasks" = "jobs"
       }
@@ -194,8 +194,8 @@ variable "nodepools_v1" {
     }
     x86userpool = {
       vm_size   = "Standard_E16as_v7"
-      min_count = 1
-      max_count = 2
+      min_count = 2
+      max_count = 16
       max_surge = "5"
     }
     monitorpool = {
